fc86e3c3e67d97572d1770dc604a019f8d4bbfb4
Jose Vara <desarrollo.vca@vcaperu.com>
2021-04-21 15:44:18 -0500

    newfunc

--- config/functions.js
+++ config/functions.js
@@ -4,4 +4,5 @@ module.exports = {
     UFN_CHATWEBAPPLICATION_INS: "select * from ufn_chatwebapplication_ins($orgid, $id, $name, $operation, $description, $type, $status, $username, $apikey)",
     UFN_CHATWEBINTEGRATION_INS: "select * from ufn_chatwebintegration_ins($chatwebapplicationid, $name, $id, $operation, $description, $type, $status, $username)",
     UFN_CHATWEBINTEGRATION_APP_SEL: "select * from ufn_chatwebintegration_app_sel($applicationid)",
+    UFN_INTEGRATION_INS: "select * from ufn_integration_ins($chatwebapplicationid, $name, $id, $operation, $description, $type, $status, $username, $color, $form, $icons)"
 }
