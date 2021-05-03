b712eda6c302e1bfc485a5a56981ca17bc9d3e79
Carlos Farro <carlos.farro@vcaperu.com>
2021-05-03 14:15:20 -0500

    upload cos

---
+++ routes/upload.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const uploadController = require("../controllers/uploadController");
+const auth = require('../middleware/auth');
+var multer = require('multer');
+var upload = multer();
+
+router.post("/",
+    auth,
+    upload.single('file'), 
+    uploadController.upload
+);
+
+
+module.exports = router;
