2a5560545d727bc53b5fea269279e82778121997
Jose Vara <desarrollo.vca@vcaperu.com>
2021-08-05 15:53:30 -0500

    funciones

--- config/functions.js
+++ config/functions.js
@@ -7,6 +7,7 @@ module.exports = {
     UFN_USER_SEL: "SELECT * FROM ufn_user_sel($corpid, $orgid, $id, $username, $all)",
     UFN_ORGUSER_SEL: "SELECT * FROM ufn_orguser_sel($corpid, $orgid, $userid, $username, $all)",
     UFN_ORGUSER_INS: "SELECT * FROM ufn_orguser_ins($corpid, $orgid, $userid, $roleid, $usersupervisor, $bydefault, $labels, $groups, $channels, $status,$type, $defaultsort, $username, $operation, $redirect)",
-    UFN_USER_INS: "SELECT * FROM ufn_user_ins($id, $usr, $doctype, $docnum, $pwd, $firstname, $lastname, $email, $pwdchangefirstlogin, $type, $status,$description, $username, $operation, $redirect, $company, $twofactorauthentication)"
-
+    UFN_USER_INS: "SELECT * FROM ufn_user_ins($id, $usr, $doctype, $docnum, $pwd, $firstname, $lastname, $email, $pwdchangefirstlogin, $type, $status,$description, $username, $operation, $redirect, $company, $twofactorauthentication)",
+    UFN_PROPERTY_SEL: "SELECT * FROM ufn_property_sel($corpid,$orgid,$id,$username,$applicationid,$all)" ,
+    UFN_PROPERTY_INS: "SELECT * FROM ufn_property_ins($corpid,$orgid,$communicationchannelid,$id,$propertyname,$propertyvalue, $description, $status,$type,$username,$operation)",
 }
