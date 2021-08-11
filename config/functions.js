0abdf3ac08ef1b766ba7904adf5448d039cf31d2
Jose Vara <desarrollo.vca@vcaperu.com>
2021-08-11 15:07:15 -0500

    UFN_WHITELIST_INS

--- config/functions.js
+++ config/functions.js
@@ -14,7 +14,7 @@ module.exports = {
     UFN_GROUPCONFIGURATION_SEL: "SELECT * FROM ufn_groupconfiguration_sel($corpid, $orgid, $id, $username, $all)",
     UFN_GROUPCONFIGURATION_INS: "SELECT * FROM ufn_groupconfiguration_ins($corpid, $orgid, $id, $operation, $domainid, $description, $type, $status, $username, $quantity, $validationtext)",
     UFN_WHITELIST_SEL: "SELECT * FROM ufn_whitelist_sel($corpid, $orgid, $username, $id, $all)",
-    UFN_WHITELIST_INS: "SELECT * FROM ufn_whitelist_ins($corpid,$orgid,$id,$operation,$documenttype,$documentnumber,$usergroup,$type,$status,$username)",
+    UFN_WHITELIST_INS: "SELECT * FROM ufn_whitelist_ins($corpid,$orgid,$id,$operation,$documenttype,$documentnumber,$usergroup,$type,$status,$asesorname,$username)",
 
     UFN_CORPBYUSER_LST: "SELECT * FROM ufn_corpbyuser_lst($userid)",
     UFN_ORGBYCORP_LST: "SELECT * FROM ufn_orgbycorp_lst($corpid)",
