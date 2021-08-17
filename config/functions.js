92eb1d119c2e69a56c25878fb7c611b67a4d02c7
Carlos Farro <carlos.farro@vcaperu.com>
2021-08-17 12:48:27 -0500

    function

--- config/functions.js
+++ config/functions.js
@@ -7,7 +7,7 @@ module.exports = {
     UFN_USER_SEL: "SELECT * FROM ufn_user_sel($corpid, $orgid, $id, $username, $all)",
     UFN_ORGUSER_SEL: "SELECT * FROM ufn_orguser_sel($corpid, $orgid, $userid, $username, $all)",
     UFN_ORGUSER_INS: "SELECT * FROM ufn_orguser_ins($corpid, $orgid, $userid, $roleid, $usersupervisor, $bydefault, $labels, $groups, $channels, $status,$type, $defaultsort, $username, $operation, $redirect)",
-    UFN_USER_INS: "SELECT * FROM ufn_user_ins($id, $usr, $doctype, $docnum, $password, $firstname, $lastname, $email, $pwdchangefirstlogin, $type, $status,$description, $username, $operation, $company, $twofactorauthentication, $registercode)",
+    UFN_USER_INS: "SELECT * FROM ufn_user_ins($id, $usr, $doctype, $docnum, $password, $firstname, $lastname, $email, $pwdchangefirstlogin, $type, $status,$description, $username, $operation, $company, $twofactorauthentication, $registercode, $billinggroup)",
     UFN_COMMUNICATIONCHANNEL_LST: "SELECT * FROM ufn_communicationchannel_lst($corpid, $orgid)",
     UFN_PROPERTY_SEL: "SELECT * FROM ufn_property_sel($corpid,$orgid,$id,$username,$all)" ,
     UFN_USER_SUPERVISOR_LST: "SELECT * FROM ufn_user_supervisor_lst($corpid, $orgid, $userid)",
