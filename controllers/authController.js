b1ac81b6844d3bc018e9e339151cbd6d2ca272c6
Carlos Farro <fdcarlosd1@gmail.com>
2021-10-04 18:59:12 -0500

    f

--- controllers/authController.js
+++ controllers/authController.js
@@ -148,7 +148,7 @@ exports.changeOrganization = async (req, res) => {
     const resultBD = await tf.executesimpletransaction("UFN_USERSTATUS_UPDATE_ORG", { ...req.user, ...parameters });
 
     if (!resultBD.error) {
-        
+        console.log(resultBD)
         const newusertoken = { ...req.user, orgid: parameters.neworgid, corpid: parameters.newcorpid, redirect: resultBD[0].redirect };
         
         const resBDMenu = await tf.executesimpletransaction("UFN_APPLICATION_SEL", newusertoken);
