6926b13118518f08b331d5adbba65b2b666fd09b
Pablo Rojas <projasb25@gmail.com>
2021-10-25 22:43:28 -0500

    ins_column

--- config/functions.js
+++ config/functions.js
@@ -1071,4 +1071,9 @@ module.exports = {
         module: "",
         protected: "SELECT"
     },
+    UFN_COLUMN_INS: {
+        query: "select * from ufn_column_ins($corpid, $orgid, $id, $description, $type, $status, $edit, $username, $index, $operation)",
+        module: "",
+        protected: "SELECT"
+    },
 }
