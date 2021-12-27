e58dde993f6c211d027b9aa19bae2dd0c599a766
Carlos Farro <carlos.farro@vcaperu.com>
2021-12-27 12:19:08 -0500

    time on each function

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -5,7 +5,7 @@ const { setSessionParameters } = require('../config/helpers');
 exports.GetCollection = async (req, res) => {
     const { parameters = {}, method } = req.body;
     setSessionParameters(parameters, req.user);
-    console.log(method, parameters)
+    // console.log(method, parameters)
     const result = await executesimpletransaction(method, parameters, req.user.menu || {});
 
     if (result instanceof Array)
@@ -39,7 +39,7 @@ exports.GetMultiDomainsValue = async (req, res) => {
                     domainname
                 }
             }))
-            console.log(detailRequest)
+            // console.log(detailRequest)
             const result = await GetMultiCollection(detailRequest);
             return res.json({ success: true, data: result });
         }
@@ -99,9 +99,9 @@ exports.export = async (req, res) => {
 
     setSessionParameters(parameters, req.user);
 
-    console.time(`exe-${method}`);
+    // console.time(`exe-${method}`);
     const resultBD = !parameters.isNotPaginated ? await buildQueryWithFilterAndSort(method, parameters) : await executesimpletransaction(method, parameters);
-    console.timeEnd(`exe-${method}`);
+    // console.timeEnd(`exe-${method}`);
 
     const result = await exportData(resultBD, parameters.reportName, parameters.formatToExport);
 
@@ -136,7 +136,7 @@ exports.getToken = async (req, res) => {
     const { data } = req.body;
     //userid
     const result = await executesimpletransaction("UFN_GET_TOKEN_LOGGED_MOVIL", data);
-    console.log(result)
+    // console.log(result)
     if (result instanceof Array) {
         if (result.length > 0) {
             return res.json({ error: false, success: true, token: result[0].token });
