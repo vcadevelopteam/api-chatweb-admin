78d4640229e9453b37ca78e6396edd1e02dc1784
Carlos Farro <carlos.farro@vcaperu.com>
2022-01-03 16:26:25 -0500

    functions

--- config/functions.js
+++ config/functions.js
@@ -1067,7 +1067,7 @@ module.exports = {
         protected: "SELECT"
     },
     UFN_PERSON_INS: {
-        query: "select * from ufn_person_ins( $id, $corpid, $orgid, $groups, $status, $type, $persontype, $personstatus, $phone, $email, $birthday, $alternativephone, $alternativeemail, $documenttype, $documentnumber, $firstname, $lastname, $sex, $gender, $civilstatus, $occupation, $educationlevel, $referringpersonid, $username, $operation)",
+        query: "select * from ufn_person_ins( $id, $corpid, $orgid, $groups, $status, $type, $persontype, $personstatus, $phone, $email, $birthday, $alternativephone, $alternativeemail, $documenttype, $documentnumber, $firstname, $lastname, $sex, $gender, $civilstatus, $occupation, $educationlevel, $referringpersonid, $observation, $username, $operation)",
         module: "",
         protected: "SELECT"
     },
