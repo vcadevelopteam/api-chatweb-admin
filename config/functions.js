6eb3f399e621a966803d71807257ecfd00e8f4b9
LuisGH1234 <u201516808@upc.edu.pe>
2022-01-13 14:57:28 -0500

    UFN_ADVISERSBYUSERID_SEL & UFN_LEAD_SEL

--- config/functions.js
+++ config/functions.js
@@ -1112,7 +1112,7 @@ module.exports = {
         protected: "SELECT"
     },
     UFN_LEAD_SEL: {
-        query: "select * from ufn_lead_sel($corpid, $orgid,  $id, $fullname, $leadproduct, $campaignid, $tags, $userid, $all)",
+        query: "select * from ufn_lead_sel($corpid, $orgid,  $id, $fullname, $leadproduct, $campaignid, $tags, $userid, $supervisorid, $all)",
         module: "",
         protected: "SELECT"
     },
@@ -1716,6 +1716,11 @@ module.exports = {
     },
     UFN_INVOICE_REFRESHTEST: {
         query: "select * from ufn_invoice_refreshtest()",
+        module: "",
+        protected: "SELECT"
+    },
+    UFN_ADVISERSBYUSERID_SEL: {
+        query: "select * from ufn_advisersbyuserid_sel($corpid, $orgid, $userid)",
         module: "",
         protected: "SELECT"
     },
