a21a882495d42da48621386d6d0687794f54859f
Diego Del Castillo <miguel.delgado@vcaperu.com>
2022-02-09 15:26:53 -0500

    Se modifico el chequeo para el plan de pago en la creación de usuarios y la inserción de billingperiod

--- config/functions.js
+++ config/functions.js
@@ -1273,7 +1273,7 @@ module.exports = {
         protected: "SELECT"
     },
     UFN_USER_PAYMENTPLAN_CHECK: {
-        query: "SELECT * FROM ufn_user_paymentplan_check($corpid)",
+        query: "SELECT * FROM ufn_user_paymentplan_check($corpid, $orgid)",
         module: "",
         protected: "SELECT"
     },
@@ -1503,7 +1503,7 @@ module.exports = {
         protected: "INSERT"
     },
     UFN_BILLINGPERIOD_UPD: {
-        query: "SELECT * FROM ufn_billingperiod_upd($corpid, $orgid, $year, $month, $billingplan, $supportplan, $basicfee, $userfreequantity, $useradditionalfee, $channelfreequantity, $channelwhatsappfee, $channelotherfee, $clientfreequantity, $clientadditionalfee, $supportbasicfee, $unitpricepersms, $vcacomissionpersms, $unitepricepermail, $vcacomissionpermail, $additionalservicename1, $additionalservicefee1, $additionalservicename2, $additionalservicefee2, $additionalservicename3, $additionalservicefee3, $freewhatsappchannel, $freewhatsappconversations, $force)",
+        query: "SELECT * FROM ufn_billingperiod_upd($corpid, $orgid, $year, $month, $billingplan, $supportplan, $basicfee, $userfreequantity, $useradditionalfee, $channelfreequantity, $channelwhatsappfee, $channelotherfee, $clientfreequantity, $clientadditionalfee, $supportbasicfee, $unitpricepersms, $vcacomissionpersms, $unitepricepermail, $vcacomissionpermail, $additionalservicename1, $additionalservicefee1, $additionalservicename2, $additionalservicefee2, $additionalservicename3, $additionalservicefee3, $freewhatsappchannel, $freewhatsappconversations, $usercreateoverride, $force)",
         module: "",
         protected: "INSERT"
     },
