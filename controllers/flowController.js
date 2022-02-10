47a7c6c9f29965d17bde58926553c41554d15ffb
Carlos Farro <carlos.farro@vcaperu.com>
2022-02-10 13:43:19 -0500

    fix location

--- controllers/flowController.js
+++ controllers/flowController.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 exports.Location = async (req, res) => {
-    const { data } = req.body;
+    const data = req.body;
 
     axios.post(`${process.env.SERVICES}handler/sendlocation`, { data });
 
