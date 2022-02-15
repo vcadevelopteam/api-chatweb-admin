04c78da524b47b8ca9b70dea53756140fec1386a
nanoxxi93 <hernando.arima@vcaperu.com>
2022-02-15 17:14:43 -0500

    main ip middleware

--- routes/main.js
+++ routes/main.js
@@ -4,44 +4,54 @@ const mainController = require("../controllers/mainController");
 const auth = require('../middleware/auth');
 
 router.post("/",
+    ip,
     auth,
     mainController.GetCollection
 )
 router.post("/public/domainvalues",
+    ip,
     mainController.GetCollectionDomainValues
 )
 router.post("/public/multi/domainvalues",
+    ip,
     mainController.GetMultiDomainsValue
 )
 router.post("/multi",
+    ip,
     auth,
     mainController.multiCollection
 )
 router.post("/executetransaction",
+    ip,
     auth,
     mainController.executeTransaction
 )
 
 router.post("/paginated",
+    ip,
     auth,
     mainController.getCollectionPagination
 )
 
 router.post("/graphic",
+    ip,
     auth,
     mainController.getGraphic
 )
 
 router.post("/export",
+    ip,
     auth,
     mainController.export
 )+
 
 router.post("/getToken",
+    ip,
     mainController.getToken
 )
 
 router.post("/validateConversationWhatsapp",
+    ip,
     mainController.validateConversationWhatsapp
 )
 
