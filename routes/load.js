8d3e67742ee807da7928a271a2247c30efde9f2d
nanoxxi93 <hernando.arima@vcaperu.com>
2022-02-15 17:24:34 -0500

    load ip middleware

--- routes/load.js
+++ routes/load.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const loadController = require("../controllers/loadController");
+const ip = require('../middleware/ip');
 
-router.post('/:table_name/:action', loadController.load)
+router.post('/:table_name/:action',
+    ip,
+    loadController.load
+)
 
 module.exports = router;
