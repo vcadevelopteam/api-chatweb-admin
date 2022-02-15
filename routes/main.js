bf76be6beed0f75a7d55f0b165f51fd72f09e2a2
nanoxxi93 <hernando.arima@vcaperu.com>
2022-02-15 17:16:37 -0500

    main fix ip middleware

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mainController = require("../controllers/mainController");
 const auth = require('../middleware/auth');
+const ip = require('../middleware/ip');
 
 router.post("/",
     ip,
