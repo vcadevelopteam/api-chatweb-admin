13ebb6aee5d18ec1e170604ec2db0123d5157168
nanoxxi93 <hernando.arima@vcaperu.com>
2022-02-15 19:05:03 -0500

    ip clean console log

--- middleware/ip.js
+++ middleware/ip.js
@@ -2,17 +2,13 @@ const ipsAllowed = (process.env.IPS_ALLOWED || '').split(',');
 
 module.exports = async function (req, res, next) {
     let origin = req.headers['origin'];
-    console.log('ips:', req.ips)
-    console.log('x-forwarded-for:', req.headers['x-forwarded-for'])
-    console.log('remoteAddress:', req.connection.remoteAddress)
-    console.log('headers', req.headers)
-
     try {
         if (!!origin) {
             next();
         }
         else {
-            console.log(req.ip)
+            console.log('ip', req.ip)
+            console.log('x-forwarded-for:', req.headers['x-forwarded-for'])
             if (ipsAllowed.includes(req.ip)) {
                 next();
             }
