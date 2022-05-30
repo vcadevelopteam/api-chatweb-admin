4635ea93bf9854bf81b96c14893910763adbbbef
Carlos Farro <carlos.farro@vcaperu.com>
2022-05-30 11:25:09 -0500

    new shipping car

--- controllers/flowController.js
+++ controllers/flowController.js
@@ -9,6 +9,23 @@ exports.Location = async (req, res) => {
     res.json({ success: true });
 }
 
+exports.ShippingCar = async (req, res) => {
+    const data = req.body;
+    data.list.forEach(x => {
+        const dd = {
+            ...data,
+            list: undefined,
+            PR_CODIGO: x.key,
+            PD_CANTIDAD: x.quantity,
+            DESCRIPCION: x.description
+        }
+        console.log(dd)
+        axios.post(`https://backend.laraigo.com/zyxme/bridge/api/processsolgas/sendrequest`, dd);
+    })
+
+    res.json({ success: true });
+}
+
 const getHttpAuthorization = (authorization) => {
     if (authorization.length > 0) {
         let auth = authorization[0];
@@ -44,33 +61,33 @@ const setConfig = (auth, headers) => {
 exports.TestRequest = async (req, res) => {
     try {
         const { method, url, authorization, headers, postformat, body, parameters } = req.body;
-        let headersjson = headers.reduce((a, x) => ({...a, [x.key]: x.value}), {});
-        let parametersjson = parameters.reduce((a, x) => ({...a, [x.key]: x.value}), {});
+        let headersjson = headers.reduce((a, x) => ({ ...a, [x.key]: x.value }), {});
+        let parametersjson = parameters.reduce((a, x) => ({ ...a, [x.key]: x.value }), {});
         let result = {}
         if (method === 'POST') {
             if (postformat.toLowerCase() === 'urlencoded') {
                 const formData = new FormData();
                 Object.keys(parametersjson).forEach(key => formData.append(key, parametersjson[key]));
-                result = await axios.post(url, formData, {...setConfig(authorization, {...formData.getHeaders(), ...headersjson})});
+                result = await axios.post(url, formData, { ...setConfig(authorization, { ...formData.getHeaders(), ...headersjson }) });
             }
             else if (postformat.toLowerCase() === 'json') {
-                result = await axios.post(url, JSON.parse(body), {...setConfig(authorization, headersjson)});
+                result = await axios.post(url, JSON.parse(body), { ...setConfig(authorization, headersjson) });
             }
             else {
-                result = await axios.post(url, body, {...setConfig(authorization, headersjson)});
+                result = await axios.post(url, body, { ...setConfig(authorization, headersjson) });
             }
         }
         else {
-            result = await axios.get(url, {...setConfig(authorization, headersjson)});
+            result = await axios.get(url, { ...setConfig(authorization, headersjson) });
         }
         return res.json(result.data);
     }
     catch (err) {
         if (!!err.response) {
-            return res.json({error: 'ERROR', status: err.response?.status, data: err.response?.data}); 
+            return res.json({ error: 'ERROR', status: err.response?.status, data: err.response?.data });
         }
         else {
-            return res.json({error: err.message}); 
+            return res.json({ error: err.message });
         }
     }
 }
