4db24893eb2ed4bd9f8869e8aaab4a2de2f458f9
nanoxxi93 <hernando.arima@vcaperu.com>
2022-06-15 15:31:32 -0500

    migratorv3

--- routes/migrator.js
+++ routes/migrator.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const migratorController = require("../controllers/migratorController");
 const migratorv2Controller = require("../controllers/migratorv2Controller");
+const migratorv3Controller = require("../controllers/migratorv3Controller");
 const auth = require('../middleware/auth');
 
 router.post('/listcorp', migratorController.listCorp)
@@ -9,5 +10,7 @@ router.post('/listcorp', migratorController.listCorp)
 router.post('/execute', migratorController.executeMigration)
 
 router.post('/executev2', migratorv2Controller.executeMigration)
+
+router.post('/executev3', migratorv3Controller.executeMigration)
 
 module.exports = router;
