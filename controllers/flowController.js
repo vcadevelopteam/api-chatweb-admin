4047e6efab90ba4b672c88165b27966174586bdf
Carlos Farro <carlos.farro@vcaperu.com>
2022-06-17 10:35:59 -0500

    fix solgas send all list

--- controllers/flowController.js
+++ controllers/flowController.js
@@ -11,22 +11,17 @@ exports.Location = async (req, res) => {
 
 exports.ShippingCar = async (req, res) => {
     const data = req.body;
-    const listreq = data.list.map(async x => {
-        const dd = {
-            ...data,
-            list: undefined,
-            PR_CODIGO: x.key,
-            PD_CANTIDAD: x.quantity,
-            DESCRIPCION: x.description
-        }
-        console.log(dd)
-        const rr = await axios.post(`https://backend.laraigo.com/zyxme/bridge/api/processsolgas/sendrequest`, dd);
-        console.log("rrcarrito", rr.data)
-        return rr.data
-    })
-    const resultReports = await Promise.all(listreq);
-    
-    res.json({ success: true, ...resultReports[0] });
+    const listreq = data.list.map(async x => ({
+        ...data,
+        list: undefined,
+        PR_CODIGO: x.key,
+        PD_CANTIDAD: x.quantity,
+        DESCRIPCION: x.description
+    }))
+    const response = await axios.post(`https://backend.laraigo.com/zyxme/bridge/api/processsolgas/sendrequestlist`, listreq);
+    console.log("rrcarrito", response.data)
+
+    res.json({ success: true, result: response.data });
 }
 
 const getHttpAuthorization = (authorization) => {
