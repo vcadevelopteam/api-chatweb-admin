ea399faa0da2caeaf28bc021c577be9ce979c310
Diego Del Castillo <diego.delcastillo@vcaperu.com>
2022-07-19 12:22:00 -0500

    RLA178. Log en exchangecode

--- controllers/googleController.js
+++ controllers/googleController.js
@@ -63,7 +63,7 @@ exports.exchangeCode = async (request, response) => {
         return response.status(500).json({
             code: "error_unexpected_error",
             error: true,
-            message: exception.message,
+            message: exception.message + ` - ${JSON.stringify(exception)}`,
             success: false,
         });
     }
