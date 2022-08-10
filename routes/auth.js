42a2e7d3afaf02117024fa82782fac4fac87022d
Carlos Farro <carlos.farro@vcaperu.com>
2022-08-10 11:25:47 -0500

    fix on auth

--- routes/auth.js
+++ routes/auth.js
@@ -2,34 +2,24 @@ const express = require("express");
 const router = express.Router();
 const auth = require('../middleware/auth');
 const authController = require("../controllers/authController")
-const ip = require('../middleware/ip');
 
 router.post("/",
-    ip,
     authController.authenticate
 )
 
 router.post("/logout",
-    ip,
     auth,
     authController.logout
 )
 
 router.post("/connect",
-    ip,
     auth,
     authController.connect
 )
 
 router.post("/changeorganization",
-    ip,
     auth,
     authController.changeOrganization
 )
 
-router.get("/",
-    ip,
-    auth,
-    authController.getUser
-)
 module.exports = router;
