a4ec68f10002d08171dfbf3e825baaf0ace8190f
Carlos Farro <carlos.farro@vcaperu.com>
2022-09-01 13:41:23 -0500

    new function ufn_conversation_callhold

--- config/functions.js
+++ config/functions.js
@@ -2586,4 +2586,9 @@ module.exports = {
         module: "/reports",
         protected: "SELECT"
     },
+    UFN_CONVERSATION_CALLHOLD: {
+        query: "SELECT * FROM ufn_conversation_callhold($corpid, $orgid, $conversationid, $holdtime)",
+        module: "",
+        protected: "SELECT"
+    },
 }
