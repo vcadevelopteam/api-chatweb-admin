cce711ad5b3cda765ad4d16113abca973886aea5
desarrollo.vca <desarrollo.vca@vcaperu.com>
2022-09-16 15:08:14 -0500

    fix

--- config/functions.js
+++ config/functions.js
@@ -2622,7 +2622,7 @@ module.exports = {
         protected: "SELECT"
     },
     UFN_WITAI_INTENT_UTTERANCE_INS: {
-        query: "SELECT * FROM ufn_witai_intent_utterance_ins($corpid, $orgid,, $name, $description, $datajson, $utterance_datajson, $operation, $username)",
+        query: "SELECT * FROM ufn_witai_intent_utterance_ins($corpid, $orgid, $name, $description, $datajson, $utterance_datajson, $operation, $username)",
         module: "",
         protected: "SELECT"
     },
