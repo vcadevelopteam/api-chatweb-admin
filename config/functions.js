6c3b3d4312afdc43bc50f0569bc4444476a5076d
Diego Del Castillo <diego.delcastillo@vcaperu.com>
2022-10-19 10:34:20 -0500

    Cambios en Product Catalog

--- config/functions.js
+++ config/functions.js
@@ -2182,7 +2182,7 @@ module.exports = {
         protected: "SELECT"
     },
     UFN_PRODUCTCATALOG_INS: {
-        query: "SELECT * FROM ufn_productcatalog_ins($corpid, $orgid, $id, $productid, $title, $link, $imagelink, $additionalimagelink, $brand, $condition, $availability, $category, $material, $color, $pattern, $currency, $price, $saleprice, $labels, $catalogid, $catalogname, $description, $status, $type, $username, $operation)",
+        query: "SELECT * FROM ufn_productcatalog_ins($corpid, $orgid, $id, $productid, $title, $link, $imagelink, $additionalimagelink, $brand, $condition, $availability, $category, $material, $color, $pattern, $currency, $price, $saleprice, $customlabel1, $customlabel2, $customlabel3, $customlabel4, $customlabel5, $labels, $catalogid, $catalogname, $description, $status, $type, $username, $operation)",
         module: "",
         protected: "INSERT"
     },
@@ -2735,5 +2735,10 @@ module.exports = {
         query: "SELECT * FROM udtt_person_pcc_import($corpid, $orgid, $table, $username)",
         module: "",
         protected: "SELECT"
+    },
+    UFN_PRODUCTCATALOG_INS_ARRAY: {
+        query: "SELECT * FROM ufn_productcatalog_ins_array($corpid, $orgid, $username, $table)",
+        module: "",
+        protected: "INSERT"
     },
 }
