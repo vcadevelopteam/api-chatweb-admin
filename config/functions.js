526621725df2a7a46d245642701d03a5e2e0e391
Jorge Rubina <jorge.rubina@vcaperu.com>
2022-11-22 10:22:59 -0500

    RLA.266 Flag de eventos futuros

--- config/functions.js
+++ config/functions.js
@@ -2279,6 +2279,7 @@ module.exports = {
                 AND cb.corpid=$corpid
                 AND cb.orgid=$orgid
                 AND cb.calendarbookingid=$calendarbookingid
+                AND cb.datestart > NOW() + cb.timezone * INTERVAL '1HOUR'
                 AND cb.status='ACTIVO'`,
         module: "",
         protected: "SELECT"
