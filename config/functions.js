73990e40da3f7791403d274a587e5f5c6fc0a743
Diego Del Castillo <diego.delcastillo@vcaperu.com>
2022-12-05 19:50:41 -0500

    RLA263. Añadido url en resultado

--- config/functions.js
+++ config/functions.js
@@ -2846,7 +2846,7 @@ module.exports = {
         protected: "SELECT"
     },
     UFN_POSTHISTORY_SEL: {
-        query: "SELECT * FROM ufn_posthistory_sel($corpid, $orgid, $communicationchannelid, $status, $type, $datestart, $dateend)",
+        query: "SELECT * FROM ufn_posthistory_sel($corpid, $orgid, $communicationchannelid, $status, $type, $publishtatus, $datestart, $dateend)",
         module: "",
         protected: "INSERT"
     },
