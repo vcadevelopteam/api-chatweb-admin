6d9f9dbc5c677ac7fa53a870b4428f31feaf433d
desarrollo.vca <desarrollo.vca@vcaperu.com>
2023-01-13 08:01:48 -0500

    UFN_CLASSIFICATION_INS modificacion

--- config/functions.js
+++ config/functions.js
@@ -576,7 +576,7 @@ module.exports = {
         protected: "SELECT"
     },
     UFN_CLASSIFICATION_INS: {
-        query: "SELECT * FROM ufn_classification_ins( $id, $corpid, $orgid, $description, $parent, $communicationchannel, $status, $type, $username, $operation, $tags,$title, $jobplan, $usergroup, $schedule)",
+        query: "SELECT * FROM ufn_classification_ins( $id, $corpid, $orgid, $description, $parent, $communicationchannel, $status, $type, $username, $operation, $tags,$title, $jobplan, $usergroup, $schedule, $metacatalogid)",
         module: "",
         protected: "INSERT"
     },
