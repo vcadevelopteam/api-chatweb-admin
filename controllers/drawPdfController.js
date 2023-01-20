668fc4f12b13fe77efde97e293cb8ec0101a1721
Carlos Farro <fdcarlosd1@gmail.com>
2023-01-19 19:23:40 -0500

    f

--- controllers/drawPdfController.js
+++ controllers/drawPdfController.js
@@ -62,7 +62,7 @@ exports.drawCardOrder = async (req, res) => {
                     unitprice: undefined,
                     detailamount: undefined,
                     orderid: `${item.orderid}`.padStart(2, "7"),
-                    createdate: new Date(item.createdate).toLocaleString("es-PE""es-PE"),
+                    createdate: new Date(item.createdate).toLocaleString("es-PE"),
                     orderamount: `${item.currency} ${formatDecimals(item.orderamount.toFixed(2))}`,
                     detail: [
                         ...(acc[`item${item.orderid}`]?.detail || []),
