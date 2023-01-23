15fdc5576e092b0d5a6c44857a88b1dfab3b4734
Carlos Farro <fdcarlosd1@gmail.com>
2023-01-23 16:20:33 -0500

    fix

--- controllers/drawPdfController.js
+++ controllers/drawPdfController.js
@@ -5,6 +5,7 @@ const { uploadBufferToCos } = require('../config/triggerfunctions');
 const logger = require('../config/winston');
 const { executesimpletransaction } = require('../config/triggerfunctions');
 const { errors, setSessionParameters, getErrorCode, formatDecimals } = require('../config/helpers');
+const { v4: uuidv4 } = require('uuid');
 
 let options = {
     format: 'A4',
@@ -32,7 +33,7 @@ exports.draw = async (req, res) => {
                         logger.child({ _requestid: req._requestid, error: { detail: error1.stack, message: error1.message } }).error(`Request to ${req.originalUrl}: ${error1.message}`);
                         return res.status(400).json(getErrorCode(errors.UNEXPECTED_ERROR));
                     }
-                    const rr = await uploadBufferToCos(req._requestid, buffer, "application/zip", `${reportname}.pdf`);
+                    const rr = await uploadBufferToCos(req._requestid, buffer, "application/x-pdf", `${reportname}.pdf`);
                     return res.json({ error: false, success: true, url: rr.url });
                 })
             }
@@ -84,13 +85,13 @@ exports.drawCardOrder = async (req, res) => {
                     return res.status(400).json(getErrorCode(errors.UNEXPECTED_ERROR));
                 } else {
                     const newdata = data.replace(/(\bdefaultheight\b)(?!.*[\r\n]*.*\1)/, "750px");
-                    
+
                     pdf.create(newdata, options).toBuffer(async (error1, buffer) => {
                         if (error1) {
                             logger.child({ _requestid: req._requestid, error: { detail: error1.stack, message: error1.message } }).error(`Request to ${req.originalUrl}: ${error1.message}`);
                             return res.status(400).json(getErrorCode(errors.UNEXPECTED_ERROR));
                         }
-                        const rr = await uploadBufferToCos(req._requestid, buffer, "application/zip", `${reportname}.pdf`);
+                        const rr = await uploadBufferToCos(req._requestid, buffer, "application/x-pdf", `${reportname}.pdf`);
                         return res.json({ error: false, success: true, url: rr.url });
                     })
                 }
@@ -104,24 +105,24 @@ exports.drawCardOrder = async (req, res) => {
 exports.drawCardDynamic = async (req, res) => {
     try {
         const { templateid, variables, corpid, orgid } = req.body;
-    
+
         const result = await executesimpletransaction("QUERY_GET_MESSAGETEMPLATE", { hsmtemplateid: templateid, corpid, orgid });
-    
+
         if (result instanceof Array) {
             if (result.length === 0) {
                 return res.json({ error: false, success: true, url: "", code: "WITHOUT-TEMPLATE" });
             } else {
                 let template = result[0].body;
-                variables.forEach(x => { template = template.replace(new RegExp(`{{${x.key}}}`, 'gi'), x.value) });
-    
+                variables.forEach(x => { template = template.replace(new RegExp(`{{${x.Key}}}`, 'gi'), x.Value) });
+
                 const data = ejs.render(template, {});
-                
-                pdf.create(data, options).toBuffer(async (error1, buffer) => {
+
+                pdf.create(data, { ...options, type: "png" }).toBuffer(async (error1, buffer) => {
                     if (error1) {
                         logger.child({ _requestid: req._requestid, error: { detail: error1.stack, message: error1.message } }).error(`Request to ${req.originalUrl}: ${error1.message}`);
                         return res.status(400).json(getErrorCode(errors.UNEXPECTED_ERROR));
                     }
-                    const rr = await uploadBufferToCos(req._requestid, buffer, "application/zip", `cardimage.pdf`);
+                    const rr = await uploadBufferToCos(req._requestid, buffer, "image/png", `cardimageaux.png`);
                     return res.json({ error: false, success: true, url: rr.url });
                 })
             }
