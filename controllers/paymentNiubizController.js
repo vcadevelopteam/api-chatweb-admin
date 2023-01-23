6e802ce0d3f6a018d038e2f091e188dc4d3851a0
Diego Del Castillo <diego.delcastillo@vcaperu.com>
2023-01-23 13:59:02 -0500

    RLA307. API para pago de Niubiz

--- controllers/paymentNiubizController.js
+++ controllers/paymentNiubizController.js
@@ -1,7 +1,7 @@
 const triggerfunctions = require('../config/triggerfunctions');
 const genericfunctions = require('../config/genericfunctions');
 
-const { getErrorCode } = require('../config/helpers');
+const { axiosObservable, getErrorCode } = require('../config/helpers');
 
 const niubizEndpoint = process.env.NIUBIZ_ENDPOINT;
 const niubizMerchantId = process.env.NIUBIZ_MERCHANTID;
@@ -9,8 +9,70 @@ const niubizPassword = process.env.NIUBIZ_PASSWORD;
 const niubizUsername = process.env.NIUBIZ_USERNAME;
 
 exports.createSessionToken = async (request, response) => {
+    var responsedata = genericfunctions.generateResponseData(request._requestid);
+
     try {
-        var responsedata = genericfunctions.generateResponseData(request._requestid);
+        const { corpid, orgid, ordercode } = request.body;
+
+        const paymentorder = await triggerfunctions.executesimpletransaction("UFN_PAYMENTORDER_SEL", { corpid: corpid, orgid: orgid, conversationid: 0, personid: 0, paymentorderid: 0, ordercode: ordercode });
+
+        if (paymentorder) {
+            if (paymentorder[0]) {
+                if (paymentorder[0].paymentstatus === 'PENDING' && paymentorder[0].expired === false) {
+                    const buff = Buffer.from(`${niubizUsername}:${niubizPassword}`, 'utf-8');
+
+                    const requestAccessToken = await axiosObservable({
+                        headers: { Authorization: `Basic ${buff.toString('base64')}` },
+                        method: 'get',
+                        url: `${niubizEndpoint}security/v1/security`,
+                        _requestid: request._requestid,
+                    });
+
+                    if (requestAccessToken.data) {
+                        var accessToken = requestAccessToken.data;
+
+                        const requestSessionToken = await axiosObservable({
+                            data: {
+                                channel: "web",
+                                amount: paymentorder[0].totalamount,
+                                antifraud: {
+                                    merchantDefineData: {
+                                        MDD4: paymentorder[0].usermail,
+                                        MDD21: 0,
+                                        MDD32: paymentorder[0].ordercode,
+                                        MDD75: "Invitado",
+                                        MDD77: 1,
+                                    }
+                                }
+                            },
+                            headers: { Authorization: accessToken },
+                            method: 'post',
+                            url: `${niubizEndpoint}ecommerce/v2/ecommerce/token/session/${niubizMerchantId}`,
+                            _requestid: request._requestid,
+                        });
+
+                        if (requestSessionToken.data) {
+                            responsedata = genericfunctions.changeResponseData(responsedata, null, { ...paymentorder[0], ...{ sessiontoken: requestSessionToken.data.sessionKey, merchantid: niubizMerchantId } }, 'success', 200, true);
+                        }
+                        else {
+                            responsedata = genericfunctions.changeResponseData(responsedata, responsedata.code, requestSessionToken.data, 'Error creating session token', responsedata.status, responsedata.success);
+                        }
+                    }
+                    else {
+                        responsedata = genericfunctions.changeResponseData(responsedata, responsedata.code, requestAccessToken.data, 'Error creating access token', responsedata.status, responsedata.success);
+                    }
+                }
+                else {
+                    responsedata = genericfunctions.changeResponseData(responsedata, responsedata.code, responsedata.data, 'Payment order expired', responsedata.status, responsedata.success);
+                }
+            }
+            else {
+                responsedata = genericfunctions.changeResponseData(responsedata, responsedata.code, responsedata.data, 'Payment order not found', responsedata.status, responsedata.success);
+            }
+        }
+        else {
+            responsedata = genericfunctions.changeResponseData(responsedata, responsedata.code, responsedata.data, 'Payment order not found', responsedata.status, responsedata.success);
+        }
 
         return response.status(responsedata.status).json(responsedata);
     }
@@ -23,8 +85,55 @@ exports.createSessionToken = async (request, response) => {
 }
 
 exports.authorizeTransaction = async (request, response) => {
+    var responsedata = genericfunctions.generateResponseData(request._requestid);
+
     try {
-        var responsedata = genericfunctions.generateResponseData(request._requestid);
+        const { tokenId, purchaseNumber, amount, currency } = request.body;
+
+        if (tokenId) {
+            const buff = Buffer.from(`${niubizUsername}:${niubizPassword}`, 'utf-8');
+
+            const requestAccessToken = await axiosObservable({
+                headers: { Authorization: `Basic ${buff.toString('base64')}` },
+                method: 'get',
+                url: `${niubizEndpoint}security/v1/security`,
+                _requestid: request._requestid,
+            });
+
+            if (requestAccessToken.data) {
+                var accessToken = requestAccessToken.data;
+
+                const requestAuthorizeTransaction = await axiosObservable({
+                    data: {
+                        channel: "web",
+                        captureType: "manual",
+                        order: {
+                            tokenId: tokenId,
+                            purchaseNumber: purchaseNumber,
+                            amount: amount,
+                            currency: currency,
+                        }
+                    },
+                    headers: { Authorization: accessToken },
+                    method: 'post',
+                    url: `${niubizEndpoint}authorization/v3/authorization/ecommerce/${niubizMerchantId}`,
+                    _requestid: request._requestid,
+                });
+
+                if (requestAuthorizeTransaction.data) {
+                    responsedata = genericfunctions.changeResponseData(responsedata, null, requestAuthorizeTransaction.data, 'success', 200, true);
+                }
+                else {
+                    responsedata = genericfunctions.changeResponseData(responsedata, responsedata.code, requestAuthorizeTransaction.data, 'Error authorizing transaction', responsedata.status, responsedata.success);
+                }
+            }
+            else {
+                responsedata = genericfunctions.changeResponseData(responsedata, responsedata.code, requestAccessToken.data, 'Error creating access token', responsedata.status, responsedata.success);
+            }
+        }
+        else {
+            responsedata = genericfunctions.changeResponseData(responsedata, responsedata.code, responsedata.data, 'Token not found', responsedata.status, responsedata.success);
+        }
 
         return response.status(responsedata.status).json(responsedata);
     }
