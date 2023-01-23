9f7d8c6043e1fd31ea7db636dc1869e1691e363a
Carlos Farro <fdcarlosd1@gmail.com>
2023-01-23 15:55:12 -0500

    fix

--- controllers/drawPdfController.js
+++ controllers/drawPdfController.js
@@ -128,7 +128,7 @@ exports.drawCardDynamic = async (req, res) => {
                         logger.child({ _requestid: req._requestid, error: { detail: error1.stack, message: error1.message } }).error(`Request to ${req.originalUrl}: ${error1.message}`);
                         return res.status(400).json(getErrorCode(errors.UNEXPECTED_ERROR));
                     }
-                    const rr = await uploadBufferToCos(req._requestid, buffer, "application/zip", `${reportname}.pdf`);
+                    const rr = await uploadBufferToCos(req._requestid, buffer, "application/zip", `cardimage.pdf`);
                     return res.json({ error: false, success: true, url: rr.url });
                 })
             }
