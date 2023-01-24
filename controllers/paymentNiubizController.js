5dbab086af69b73953b116f3334e402b05215749
Diego Del Castillo <diego.delcastillo@vcaperu.com>
2023-01-23 19:31:21 -0500

    RLA307. API para pago de Niubiz

--- controllers/paymentNiubizController.js
+++ controllers/paymentNiubizController.js
@@ -88,51 +88,91 @@ exports.authorizeTransaction = async (request, response) => {
     var responsedata = genericfunctions.generateResponseData(request._requestid);
 
     try {
-        const { tokenId, purchaseNumber, amount, currency } = request.body;
+        const { transactionToken, customerEmail } = request.body;
+        const { corpid, orgid, ordercode, totalamount } = request.query;
 
-        if (tokenId) {
-            const buff = Buffer.from(`${niubizUsername}:${niubizPassword}`, 'utf-8');
+        if (transactionToken) {
+            const paymentorder = await triggerfunctions.executesimpletransaction("UFN_PAYMENTORDER_SEL", { corpid: corpid, orgid: orgid, conversationid: 0, personid: 0, paymentorderid: 0, ordercode: ordercode });
 
-            const requestAccessToken = await axiosObservable({
-                headers: { Authorization: `Basic ${buff.toString('base64')}` },
-                method: 'get',
-                url: `${niubizEndpoint}security/v1/security`,
-                _requestid: request._requestid,
-            });
+            if (paymentorder) {
+                if (paymentorder[0]) {
+                    if (paymentorder[0].paymentstatus === 'PENDING' && paymentorder[0].expired === false && paymentorder[0].totalamount === parseFloat(totalamount || 0)) {
+                        const buff = Buffer.from(`${niubizUsername}:${niubizPassword}`, 'utf-8');
 
-            if (requestAccessToken.data) {
-                var accessToken = requestAccessToken.data;
+                        const requestAccessToken = await axiosObservable({
+                            headers: { Authorization: `Basic ${buff.toString('base64')}` },
+                            method: 'get',
+                            url: `${niubizEndpoint}security/v1/security`,
+                            _requestid: request._requestid,
+                        });
 
-                const requestAuthorizeTransaction = await axiosObservable({
-                    data: {
-                        channel: "web",
-                        captureType: "manual",
-                        order: {
-                            tokenId: tokenId,
-                            purchaseNumber: purchaseNumber,
-                            amount: amount,
-                            currency: currency,
+                        if (requestAccessToken.data) {
+                            var accessToken = requestAccessToken.data;
+
+                            const requestAuthorizeTransaction = await axiosObservable({
+                                data: {
+                                    channel: "web",
+                                    captureType: "manual",
+                                    order: {
+                                        tokenId: transactionToken,
+                                        purchaseNumber: paymentorder[0].paymentorderid,
+                                        amount: paymentorder[0].totalamount,
+                                        currency: 'PEN',
+                                    }
+                                },
+                                headers: { Authorization: accessToken },
+                                method: 'post',
+                                url: `${niubizEndpoint}authorization/v3/authorization/ecommerce/${niubizMerchantId}`,
+                                _requestid: request._requestid,
+                            });
+
+                            if (requestAuthorizeTransaction.data) {
+                                const chargedata = await insertCharge(corpid, orgid, paymentorder[0].paymentorderid, null, totalamount, true, requestAuthorizeTransaction.data, requestAuthorizeTransaction.data.header?.ecoreTransactionUUID, paymentorder[0].currency, paymentorder[0].description, customerEmail, 'INSERT', null, null, 'API', 'PAID', accessToken, null, 'NIUBIZ', responsedata.id);
+
+                                const queryParameters = {
+                                    corpid: corpid,
+                                    orgid: orgid,
+                                    paymentorderid: paymentorder[0].paymentorderid,
+                                    paymentby: customerEmail || paymentorder[0].personid,
+                                    culqiamount: totalamount,
+                                    chargeid: chargedata?.chargeid || null,
+                                    chargetoken: requestAuthorizeTransaction.data.header?.ecoreTransactionUUID || null,
+                                    chargejson: requestAuthorizeTransaction.data || null,
+                                    tokenid: accessToken,
+                                    tokenjson: null,
+                                };
+
+                                const paymentResult = await triggerfunctions.executesimpletransaction("UFN_PAYMENTORDER_PAYMENT", queryParameters);
+
+                                if (paymentResult instanceof Array) {
+                                    responsedata = genericfunctions.changeResponseData(responsedata, null, { message: 'success' }, 'success', 200, true);
+                                }
+                                else {
+                                    responsedata = genericfunctions.changeResponseData(responsedata, responsedata.code, responsedata.data, 'Insert failure', responsedata.status, responsedata.success);
+                                }
+                            }
+                            else {
+                                responsedata = genericfunctions.changeResponseData(responsedata, responsedata.code, requestAuthorizeTransaction.data, 'Error authorizing transaction', responsedata.status, responsedata.success);
+                            }
                         }
-                    },
-                    headers: { Authorization: accessToken },
-                    method: 'post',
-                    url: `${niubizEndpoint}authorization/v3/authorization/ecommerce/${niubizMerchantId}`,
-                    _requestid: request._requestid,
-                });
-
-                if (requestAuthorizeTransaction.data) {
-                    responsedata = genericfunctions.changeResponseData(responsedata, null, requestAuthorizeTransaction.data, 'success', 200, true);
+                        else {
+                            responsedata = genericfunctions.changeResponseData(responsedata, responsedata.code, requestAccessToken.data, 'Error creating access token', responsedata.status, responsedata.success);
+                        }
+                    }
+                    else {
+                        responsedata = genericfunctions.changeResponseData(responsedata, responsedata.code, responsedata.data, 'Payment order expired', responsedata.status, responsedata.success);
+                    }
                 }
                 else {
-                    responsedata = genericfunctions.changeResponseData(responsedata, responsedata.code, requestAuthorizeTransaction.data, 'Error authorizing transaction', responsedata.status, responsedata.success);
+                    responsedata = genericfunctions.changeResponseData(responsedata, responsedata.code, responsedata.data, 'Payment order not found', responsedata.status, responsedata.success);
                 }
             }
             else {
-                responsedata = genericfunctions.changeResponseData(responsedata, responsedata.code, requestAccessToken.data, 'Error creating access token', responsedata.status, responsedata.success);
+                responsedata = genericfunctions.changeResponseData(responsedata, responsedata.code, responsedata.data, 'Payment order not found', responsedata.status, responsedata.success);
             }
         }
         else {
-            responsedata = genericfunctions.changeResponseData(responsedata, responsedata.code, responsedata.data, 'Token not found', responsedata.status, responsedata.success);
+            responsedata = genericfunctions.changeResponseData(responsedata, responsedata.code, responsedata.data, 'Transaction token not found', responsedata.status, responsedata.success);
         }
 
         return response.status(responsedata.status).json(responsedata);
@@ -143,4 +183,39 @@ exports.authorizeTransaction = async (request, response) => {
             message: exception.message,
         });
     }
+}
+
+const insertCharge = async (corpId, orgId, paymentorderid, id, amount, capture, chargeJson, chargeToken, currency, description, email, operation, orderId, orderJson, paidBy, status, tokenId, tokenJson, type, requestId) => {
+    const queryParameters = {
+        amount: amount,
+        capture: capture,
+        chargejson: chargeJson,
+        chargetoken: chargeToken,
+        corpid: corpId,
+        currency: currency,
+        description: description,
+        email: email,
+        id: id,
+        paymentorderid: paymentorderid,
+        operation: operation,
+        orderid: orderId,
+        orderjson: orderJson,
+        orgid: orgId,
+        paidby: paidBy,
+        status: status,
+        tokenid: tokenId,
+        tokenjson: tokenJson,
+        type: type,
+        _requestid: requestId,
+    }
+
+    const queryResult = await triggerfunctions.executesimpletransaction("UFN_CHARGE_PAYMENTORDER_INS", queryParameters);
+
+    if (queryResult instanceof Array) {
+        if (queryResult.length > 0) {
+            return queryResult[0];
+        }
+    }
+
+    return null;
 }
--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const allowedOrigins = process.env.ADDRESSES_ALLOWED?.split(",") || [];
 
 const app = express();
 
+app.use(express.urlencoded({
+    extended: true
+}))
+
 app.use(morganMiddleware);
 
 app.use(cors({
@@ -18,8 +22,7 @@ app.use(cors({
         if (!origin) return callback(null, true);
         if (allowedOrigins.indexOf(origin) === -1) {
             // console.log(`${dateRequest}: not allowed from ${origin}`)
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
+            var msg = `The CORS policy for this site does not allow access from the specified Origin (${origin}).`;
             return callback(new Error(msg), false);
         }
         return callback(null, true); amara
