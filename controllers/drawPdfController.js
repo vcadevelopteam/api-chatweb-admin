f70258a3f2ffaee9a580339a66b4f063b6705ec6
Carlos Farro <fdcarlosd1@gmail.com>
2023-04-05 15:23:41 -0500

    avance

--- controllers/drawPdfController.js
+++ controllers/drawPdfController.js
@@ -133,3 +133,32 @@ exports.drawCardDynamic = async (req, res) => {
         return getErrorCode(errors.UNEXPECTED_ERROR, exception, "Executing drawCardDynamic");
     }
 }
+
+//corpid, orgid, personid, limit
+exports.drawPDFSBS = async (req, res) => {
+    const { parameters } = req.body;
+
+    try {
+        ejs.renderFile(path.join('./views/', "card-order.html"), {
+            data: parameters
+        }, (error, data) => {
+            if (error) {
+                logger.child({ _requestid: req._requestid, error: { detail: error.stack, message: error.message } }).error(`Request to ${req.originalUrl}: ${error.message}`);
+                return res.status(400).json(getErrorCode(errors.UNEXPECTED_ERROR));
+            } else {
+                pdf.create(data, options).toBuffer(async (error1, buffer) => {
+                    if (error1) {
+                        logger.child({ _requestid: req._requestid, error: { detail: error1.stack, message: error1.message } }).error(`Request to ${req.originalUrl}: ${error1.message}`);
+                        return res.status(400).json(getErrorCode(errors.UNEXPECTED_ERROR));
+                    }
+                    const rr = await uploadBufferToCos(req._requestid, buffer, "application/x-pdf", `${reportname}.pdf`);
+                    return res.json({ error: false, success: true, url: rr.url });
+                })
+            }
+        });
+    } catch (exception) {
+        return getErrorCode(errors.UNEXPECTED_ERROR, exception, "Executing drawCardDynamic");
+    }
+
+
+}
