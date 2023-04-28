14a1811e05ebe27b70c13bb5267356363b6ded46
Diego Del Castillo <diego.delcastillo@vcaperu.com>
2023-04-28 14:26:45 -0500

    RLAEN002. Añadida opcion de LinkedIn

--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -11,8 +11,13 @@ const cryptojs = require("crypto-js");
 const bridgeEndpoint = process.env.BRIDGE;
 const brokerEndpoint = process.env.CHATBROKER;
 const facebookEndpoint = process.env.FACEBOOKAPI;
+const googleClientId = process.env.GOOGLE_CLIENTID;
+const googleClientSecret = process.env.GOOGLE_CLIENTSECRET;
+const googleTopicName = process.env.GOOGLE_TOPICNAME;
 const hookEndpoint = process.env.HOOK;
 const laraigoEndpoint = process.env.LARAIGO;
+const linkedinEndpoint = process.env.LINKEDIN;
+const linkedinTokenEndpoint = process.env.LINKEDINTOKEN;
 const smoochEndpoint = process.env.SMOOCHAPI;
 const smoochVersion = process.env.SMOOCHVERSION;
 const telegramEndpoint = process.env.TELEGRAMAPI;
@@ -20,9 +25,6 @@ const userSecret = process.env.USERSECRET;
 const webChatApplication = process.env.CHATAPPLICATION;
 const webChatScriptEndpoint = process.env.WEBCHATSCRIPT;
 const whitelist = process.env.WHITELIST;
-const googleClientId = process.env.GOOGLE_CLIENTID;
-const googleClientSecret = process.env.GOOGLE_CLIENTSECRET;
-const googleTopicName = process.env.GOOGLE_TOPICNAME;
 
 exports.activateUser = async (request, response) => {
     try {
@@ -915,6 +917,28 @@ exports.createSubscription = async (request, response) => {
                                 break;
 
                             case 'LINKEDIN':
+                                if (channelService) {
+                                    channelParameters.communicationchannelowner = channelService.clientid;
+                                    channelParameters.communicationchannelsite = channelService.clientid;
+                                    channelParameters.integrationid = channelService.organizationid;
+                                    channelParameters.servicecredentials = JSON.stringify({
+                                        clientId: channelService.clientid,
+                                        clientSecret: channelService.clientsecret,
+                                        endpoint: linkedinEndpoint,
+                                        organizationId: channelService.organizationid,
+                                    });
+                                    channelParameters.status = 'ACTIVO';
+                                    channelParameters.type = 'LNKD';
+
+                                    await channelfunctions.serviceTokenUpdate(channelService.clientid, channelService.accesstoken, channelService.refreshtoken, JSON.stringify({ clientId: channelService.clientid, clientSecret: channelService.clientsecret, endpoint: linkedinTokenEndpoint }), 'LINKEDIN', 'ACTIVO', request?.user?.usr, 1400);
+
+                                    channelMethodArray.push(channelMethod);
+                                    channelParametersArray.push(channelParameters);
+                                    channelServiceArray.push(channelService);
+                                    channelTypeArray.push(channel.type);
+                                }
+                                break;
+
                             case 'MICROSOFTTEAMS':
                             case 'TIKTOK':
                                 if (channelService) {
