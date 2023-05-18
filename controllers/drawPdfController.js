11a5ee5a7145a3082493a76f7f279d6aa0a5540e
kevjp095 <kevin.ramos@vcaperu.com>
2023-05-18 17:21:37 -0500

    fixed name file

--- controllers/drawPdfController.js
+++ controllers/drawPdfController.js
@@ -162,10 +162,10 @@ exports.drawPDFSBS = async (req, res) => {
                         logger.child({ _requestid: req._requestid, error: { detail: error1.stack, message: error1.message } }).error(`Request to ${req.originalUrl}: ${error1.message}`);
                         return res.status(400).json(getErrorCode(errors.UNEXPECTED_ERROR));
                     }
-                    //const fileName = "Reporte de Deudas";
-                    //const encodedFileName = encodeURIComponent(fileName);
+                    const fileName = "Reporte de Deudas";
+                    const encodedFileName = encodeURIComponent(fileName);
 
-                    const rr = await uploadBufferToCos(req._requestid, buffer, "application/x-pdf", `${uuidv4()}/reporte.pdf`);
+                    const rr = await uploadBufferToCos(req._requestid, buffer, "application/x-pdf", `${uuidv4()}/${encodedFileName}`);
                     return res.json({ error: false, success: true, url: rr.url });
                 })
             }
