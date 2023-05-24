3be693fc99f2a1788e26e591c1009ae647f1c918
Carlos Farro <fdcarlosd1@gmail.com>
2023-05-24 18:58:40 -0500

    avance

--- controllers/backupController.js
+++ controllers/backupController.js
@@ -3,18 +3,18 @@ const { errors, getErrorCode, axiosObservable } = require('../config/helpers');
 const { executesimpletransaction: exeMethod, executeQuery } = require('../config/triggerfunctions');
 const backupSeq = require("../config/databasebackup");
 const sequelize = require('../config/database');
-const { Pool } = require('pg')
-const Cursor = require('pg-cursor')
+const { Pool } = require('pg');
+const Cursor = require('pg-cursor');
 const BATCH_SIZE = 100_000;
 
-const processCursor = async (cursor, tablename, type, columnpk, client) => {
+const processCursor = async (cursor, tablename, type, columnpk, client, lastdate) => {
     let indexPart = 1;
 
     return new Promise((resolve, reject) => {
         (function read() {
             cursor.read(BATCH_SIZE, async (err, rows) => {
                 if (err) {
-                    logger.error({ error: err })
+                    logger.error({ error: err });
                     return resolve({ error: true, err });
                 }
                 // no more rows, so we're done!
@@ -22,21 +22,21 @@ const processCursor = async (cursor, tablename, type, columnpk, client) => {
                     return resolve({ error: false, success: true });
                 }
 
-                console.log(`${tablename}|part ${indexPart}|${rows.length}`)
-                const query = toQuery(rows, tablename, type, columnpk, client);
+                console.log(`${tablename}|part ${indexPart}|${rows.length}`);
+                const query = toQuery(rows, lastdate, tablename, type, columnpk, client);
 
                 const stream = fs.createWriteStream(`./queries/${tablename}-${indexPart}.sql`);
 
                 stream.write(query.insert, 'utf8', (error) => {
                     if (error) {
-                        return res.status(200).json({ error: false, success: false, message: "there are not tables" });
+                        return reject(error);
                     }
                     stream.end();
                 });
 
                 // Evento 'finish' se dispara cuando se completa la escritura del archivo
                 stream.on('finish', () => {
-                    // return res.status(200).json({ error: false, success: true, query: queryGG });
+                    // Aquí puedes realizar alguna acción después de completar la escritura del archivo
                 });
 
                 indexPart++;
@@ -45,7 +45,7 @@ const processCursor = async (cursor, tablename, type, columnpk, client) => {
             });
         })();
     });
-}
+};
 
 const connectX = async (query) => {
     const pool = new Pool({
@@ -58,106 +58,104 @@ const connectX = async (query) => {
         idleTimeoutMillis: 30000,
         allowExitOnIdle: true,
         ssl: {
-            rejectUnauthorized: false
-        }
-    })
+            rejectUnauthorized: false,
+        },
+    });
     return await pool.connect();
-}
-exports.incremental = async (req, res) => {
-    // const { data: { usr, password, facebookid, googleid } } = req.body;
+};
 
+exports.incremental = async (req, res) => {
     try {
         const tablesToBackup = await exeMethod("QUERY_SEL_TABLESETTING_BACKUP", {});
-        // const infoSelect = await exeMethod("QUERY_SEL_TABLESETTING_BACKUP", {});
         const infoSelect = [{
             lastdate: "2023-05-23 15:00:00",
             todate: "2023-05-23 16:00:00",
             interval: 1,
         }];
 
-        if (!(tablesToBackup instanceof Array)) {
+        if (!Array.isArray(tablesToBackup) || !Array.isArray(infoSelect)) {
             return res.status(400).json(getErrorCode(errors.UNEXPECTED_ERROR));
         }
-        if (!(infoSelect instanceof Array)) {
-            return res.status(400).json(getErrorCode(errors.UNEXPECTED_ERROR));
-        }
+
         const { lastdate, interval, todate } = infoSelect[0];
 
-        if (tablesToBackup.length == 0) {
+        if (tablesToBackup.length === 0) {
             return res.status(200).json({ error: false, success: false, message: "there are not tables" });
         }
-        
+
         for (const table of tablesToBackup) {
             const { tablename, type: typeFromBD, columnpk } = table;
-            if (!(["conversation", "interaction"].includes(tablename))) {
-                continue;
-            }
+            // if (!["conversation", "interaction"].includes(tablename)) {
+            //     continue;
+            // }
             let querySelect = "";
-            let type = ""
+            let type = "";
             if (typeFromBD === "NINGUNO") {
                 querySelect = `
-                    SELECT tb.* FROM "${tablename}" tb
-                    WHERE tb.changedate > '${lastdate}'
-                    AND tb.changedate <= '${todate}'
+                SELECT tb.* FROM "${tablename}" tb
+                WHERE tb.changedate > '${lastdate}'
+                AND tb.changedate <= '${todate}'
                 `;
             } else {
                 const info = typeFromBD.split(":");
-                type = info[0]
+                type = info[0];
                 if (info[0] === "WHERE") {
                     querySelect = `
                         SELECT tb.* 
                         FROM "${tablename}" tb
-                        WHERE ${info[1].toLowerCase()} > '${lastdate}'
-                        AND ${info[1].toLowerCase()} <= '${todate}'`;
-                    // querySelect = `
-                    //     SELECT tb.* 
-                    //     FROM "${tablename}" tb
-                    //     WHERE conversationid = 3618603944077`;
+                        WHERE conversationid = 3618603944077`;
+            //         querySelect = `
+            // SELECT tb.* 
+            // FROM "${tablename}" tb
+            // WHERE ${info[1].toLowerCase()} > '${lastdate}'
+            // AND ${info[1].toLowerCase()} <= '${todate}'`;
                 } else if (info[0] === "LASTID") {
                     querySelect = `
-                        SELECT tb.* 
-                        FROM "${tablename}" tb
-                        WHERE ${columnpk} > '${info[1]}'`;
+            SELECT tb.* 
+            FROM "${tablename}" tb
+            WHERE ${columnpk} > '${info[1]}'`;
                 }
             }
-            console.time(tablename)
+
+            console.time(tablename);
 
             const client = await connectX(querySelect);
-
             const cursor = client.query(new Cursor(querySelect));
 
-            await processCursor(cursor, tablename, type, columnpk, client)
+            await processCursor(cursor, tablename, type, columnpk, client, lastdate);
             console.timeEnd(tablename);
+
             if (type === "LASTID") {
-
+                // Realiza alguna acción adicional si el tipo es "LASTID"
             }
-        }
 
+            return res.status(200).json({ success: true });
+
+        }
     } catch (exception) {
-        console.log("aaaa")
+        console.log("aaaa");
         return res.status(500).json(getErrorCode(null, exception, `Request to ${req.originalUrl}`, req._requestid));
     }
-}
+};
 
-const toQuery = (data, tablename, type = "NINGUNO", columnpk, client) => {
+const toQuery = (data, lastdate, tablename, type = "NINGUNO", columnpk, client) => {
     const newData = data.reduce((acc, item) => {
-        const isNew = type !== "NINGUNO" || item.changedate > item.createdate && item.createdate < lastdate;
+        const isNew = type !== "NINGUNO" || (item.changedate > item.createdate && item.createdate < lastdate);
         if (isNew) {
-            console.log(client)
             const values = Object.values(item).map(value => {
-                return typeof value === "string" ? `'${value.replace(/[\n'"]/gi, '')}'` : JSON.stringify(value)
+                return typeof value === "string" ? `'${value.replace(/[\n'"]/gi, '')}'` : JSON.stringify(value);
             }).join(', ');
-            acc.insert += `(${values}),\n`
+            acc.insert += `(${values}),\n`;
             return acc;
         } else {
-            //Por desarrollar
+            // Por desarrollar
         }
     }, {
         insert: `INSERT INTO ${tablename} (${Object.keys(data[0]).join(", ")}) VALUES\n`,
-        update: `UPDATE ${tablename} SET `
+        update: `UPDATE ${tablename} SET `,
     });
 
     newData.insert = newData.insert ? newData.insert.slice(0, -2) : "";
     // newData.update = newData.update ? newData.update.slice(0, -2) : "";
     return newData;
-}
+};
