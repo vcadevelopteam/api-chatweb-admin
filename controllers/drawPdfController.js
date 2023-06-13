22e1e75011122f7368e45d749ac0d6b6540e57f1
Diego Del Castillo <diego.delcastillo@vcaperu.com>
2023-06-13 16:16:58 -0500

    Demo Edwin

--- controllers/drawPdfController.js
+++ controllers/drawPdfController.js
@@ -53,6 +53,12 @@ exports.drawCardOrder = async (req, res) => {
         if (result.length === 0) {
             return res.json({ error: false, success: true, url: "", code: "WITHOUT-ORDERS" });
         } else {
+            let date = new Date();
+            date.setHours(date.getHours() - 5);
+
+            let orderidentifier = result[0].orderid;
+            let currentdate = `${date.getDate()}`.padStart(2, '0') + '.' + `${date.getMonth() + 1}`.padStart(2, '0') + '.' + `${date.getFullYear()}`.substring(2);
+
             const ff = result.reduce((acc, item) => ({
                 ...acc,
                 [`item${item.orderid}`]: {
@@ -91,7 +97,7 @@ exports.drawCardOrder = async (req, res) => {
                             logger.child({ _requestid: req._requestid, error: { detail: error1.stack, message: error1.message } }).error(`Request to ${req.originalUrl}: ${error1.message}`);
                             return res.status(400).json(getErrorCode(errors.UNEXPECTED_ERROR));
                         }
-                        const rr = await uploadBufferToCos(req._requestid, buffer, "application/x-pdf", `${reportname}.pdf`);
+                        const rr = await uploadBufferToCos(req._requestid, buffer, "application/x-pdf", `${(reportname === 'lastorders' ? `orden-${currentdate}.${orderidentifier}` : reportname)}.pdf`);
                         return res.json({ error: false, success: true, url: rr.url });
                     })
                 }
