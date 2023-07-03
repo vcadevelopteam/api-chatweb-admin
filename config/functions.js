4ebc089cf01eb0a9527102d1660e477769d25e64
Jose Vara <josecarlosvaraquispe@gmail.com>
2023-07-03 17:43:05 -0500

    test

--- config/functions.js
+++ config/functions.js
@@ -3737,5 +3737,9 @@ module.exports = {
         query: "SELECT * FROM ufn_rasa_file_upload($corpid, $orgid, $rasaid, $intents, $synonyms, $usr)",
         protected: "SELECT"
     },
+    UFN_RASA_MODEL_SEL: {
+        query: "SELECT * FROM ufn_rasa_model_sel($corpid, $orgid)",
+        protected: "SELECT"
+    },
     
 }
