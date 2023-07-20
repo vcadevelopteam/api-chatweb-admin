e269b055111ca0d464341f680a61a98a2f8b2c3c
Jose Vara <josecarlosvaraquispe@gmail.com>
2023-07-20 14:38:54 -0500

    at1

--- config/functions.js
+++ config/functions.js
@@ -1,14 +1,14 @@
 module.exports = {
     QUERY_AUTHENTICATED: {
         query: `
-        SELECT us.company, us.pwdchangefirstlogin, org.description orgdesc, corp.description corpdesc, ous.corpid, ous.orgid, us.userid, us.usr, us.pwd, us.image, us.firstname, us.lastname, us.email, us.status, ous.groups, ous.redirect,pp.plan, role.description roledesc, COALESCE(cur.symbol, 'S/') currencysymbol, COALESCE(org.country, 'PE') countrycode, corp.paymentmethod, cc.communicationchannelsite sitevoxi, cc.communicationchannelowner ownervoxi, cc.communicationchannelid ccidvoxi, cc.voximplantcallsupervision
+        SELECT us.company, us.pwdchangefirstlogin, org.description orgdesc, corp.description corpdesc, ous.corpid, ous.orgid, us.userid, us.usr, us.pwd, us.image, us.firstname, us.lastname, us.email, us.status, ous.groups, ous.redirect,pp.plan, string_agg(role.description,',') roledesc, COALESCE(cur.symbol, 'S/') currencysymbol, COALESCE(org.country, 'PE') countrycode, corp.paymentmethod, cc.communicationchannelsite sitevoxi, cc.communicationchannelowner ownervoxi, cc.communicationchannelid ccidvoxi, cc.voximplantcallsupervision
         FROM usr us 
         INNER JOIN orguser ous ON ous.userid = us.userid 
         INNER JOIN org org ON org.orgid = ous.orgid 
         LEFT JOIN currency cur ON cur.code = org.currency 
         INNER JOIN corp corp ON corp.corpid = ous.corpid 
         LEFT JOIN paymentplan pp ON pp.paymentplanid = corp.paymentplanid 
-        INNER JOIN role role ON role.roleid = ous.roleid 
+        INNER JOIN role role ON role.roleid = any(string_to_array(ous.rolegroups, ',')::bigint[])
         LEFT JOIN communicationchannel cc ON cc.corpid = ous.corpid AND cc.communicationchannelid = ANY(string_to_array(ous.channels,',')::BIGINT[]) AND cc.orgid = ous.orgid AND cc.type = 'VOXI' AND cc.status = 'ACTIVO'
         WHERE us.usr = $usr AND ous.bydefault 
         AND ous.status <> 'ELIMINADO' 
@@ -23,14 +23,14 @@ module.exports = {
     },
     QUERY_AUTHENTICATED_BY_FACEBOOKID: {
         query: `
-        SELECT us.company, us.pwdchangefirstlogin, org.description orgdesc, corp.description corpdesc, ous.corpid, ous.orgid, us.userid, us.usr, us.pwd, us.firstname, us.image, us.lastname, us.email, us.status, ous.groups, ous.redirect, pp.plan, role.description roledesc, COALESCE(cur.symbol, 'S/') currencysymbol, COALESCE(org.country, 'PE') countrycode, corp.paymentmethod, cc.communicationchannelsite sitevoxi, cc.communicationchannelowner ownervoxi, cc.communicationchannelid ccidvoxi, cc.voximplantcallsupervision
+        SELECT us.company, us.pwdchangefirstlogin, org.description orgdesc, corp.description corpdesc, ous.corpid, ous.orgid, us.userid, us.usr, us.pwd, us.firstname, us.image, us.lastname, us.email, us.status, ous.groups, ous.redirect, pp.plan,  string_agg(role.description,',') roledesc, COALESCE(cur.symbol, 'S/') currencysymbol, COALESCE(org.country, 'PE') countrycode, corp.paymentmethod, cc.communicationchannelsite sitevoxi, cc.communicationchannelowner ownervoxi, cc.communicationchannelid ccidvoxi, cc.voximplantcallsupervision
         from usr us 
         INNER JOIN orguser ous on ous.userid = us.userid 
         INNER JOIN org org on org.orgid = ous.orgid 
         LEFT JOIN currency cur on cur.code = org.currency 
         INNER JOIN corp corp on corp.corpid = ous.corpid 
         LEFT JOIN paymentplan pp ON pp.paymentplanid = corp.paymentplanid 
-        INNER JOIN role role on role.roleid = ous.roleid 
+        INNER JOIN role role ON role.roleid = any(string_to_array(ous.rolegroups, ',')::bigint[])
         LEFT JOIN communicationchannel cc ON cc.corpid = ous.corpid AND cc.orgid = ous.orgid AND cc.type = 'VOXI' AND us.status = 'ACTIVO'
         WHERE us.facebookid = $facebookid 
         AND ous.bydefault 
@@ -41,17 +41,24 @@ module.exports = {
     },
     QUERY_AUTHENTICATED_BY_GOOGLEID: {
         query: `
-        SELECT us.company, us.pwdchangefirstlogin, org.description orgdesc, corp.description corpdesc, ous.corpid, ous.orgid, us.userid, us.usr, us.pwd, us.firstname, us.image, us.lastname, us.email, us.status, ous.groups, ous.redirect, pp.plan, role.description roledesc, COALESCE(cur.symbol, 'S/') currencysymbol, COALESCE(org.country, 'PE') countrycode, corp.paymentmethod, cc.communicationchannelsite sitevoxi, cc.communicationchannelowner ownervoxi, cc.communicationchannelid ccidvoxi, cc.voximplantcallsupervision
-        from usr us 
-        INNER JOIN orguser ous on ous.userid = us.userid 
-        INNER JOIN org org on org.orgid = ous.orgid left join currency cur on cur.code = org.currency 
-        INNER JOIN corp corp on corp.corpid = ous.corpid LEFT JOIN paymentplan pp ON pp.paymentplanid = corp.paymentplanid 
-        INNER JOIN role role on role.roleid = ous.roleid
+        SELECT us.company, us.pwdchangefirstlogin, org.description orgdesc, corp.description corpdesc, ous.corpid,
+        ous.orgid, us.userid, us.usr, us.pwd,
+        us.firstname, us.image, us.lastname, us.email, us.status,
+        ous.groups, ous.redirect, pp.plan, COALESCE(cur.symbol, 'S/') currencysymbol,
+        COALESCE(org.country, 'PE') countrycode, corp.paymentmethod, cc.communicationchannelsite sitevoxi,
+        cc.communicationchannelowner ownervoxi, cc.communicationchannelid ccidvoxi, cc.voximplantcallsupervision,
+        string_agg(role.description,',') roledesc
+        from usr us
+        INNER JOIN orguser ous on ous.userid = us.userid
+        INNER JOIN org org on org.orgid = ous.orgid left join currency cur on cur.code = org.currency
+        INNER JOIN corp corp on corp.corpid = ous.corpid LEFT JOIN paymentplan pp ON pp.paymentplanid = corp.paymentplanid
+        INNER JOIN role role on role.roleid = any(string_to_array(ous.rolegroups, ',')::bigint[])
         LEFT JOIN communicationchannel cc ON cc.corpid = ous.corpid AND cc.orgid = ous.orgid AND cc.type = 'VOXI' AND cc.status = 'ACTIVO'
-        WHERE us.googleid = $googleid 
-        AND ous.bydefault 
-        AND ous.status <> 'ELIMINADO' 
-        LIMIT 1`,
+        WHERE us.googleid = $googleid
+        AND ous.bydefault
+        AND ous.status <> 'ELIMINADO'
+		group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24
+		LIMIT 1`,
         module: "",
         protected: false
     },
