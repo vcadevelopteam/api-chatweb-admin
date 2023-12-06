c1b3ac461c75f09e38d3aecde4aa64e4076aaee2
Graciela Sánchez <oti160@oefa.gob.pe>
2023-12-06 10:54:12 -0500

    remove storeid and warehouseid

--- config/functions.js
+++ config/functions.js
@@ -103,7 +103,7 @@ module.exports = {
         protected: "SELECT"
     },
     UFN_ORGUSER_INS: {
-        query: "SELECT * FROM ufn_orguser_ins($corpid, $orgid, $p_userid, $rolegroups, $usersupervisor, $bydefault, $labels, $groups, $channels, $status,$type, $defaultsort, $username, $operation, $redirect, $storeid, $warehouseid)",
+        query: "SELECT * FROM ufn_orguser_ins($corpid, $orgid, $p_userid, $rolegroups, $usersupervisor, $bydefault, $labels, $groups, $channels, $status,$type, $defaultsort, $username, $operation, $redirect, $showbots)",
         module: "/extras/users",
         protected: "INSERT"
     },
