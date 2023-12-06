6cb0570833844efff968e1f9b5c8b191c4c16471
Diego Del Castillo <diego.delcastillo@vcaperu.com>
2023-12-05 19:14:55 -0500

    RLANOR001. Izipay metodo de pago

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ app.use('/api/backup', require('./routes/backup'));
 app.use('/api/payment', require('./routes/payment'));
 app.use('/api/paymentniubiz', require('./routes/paymentniubiz'));
 app.use('/api/paymentopenpay', require('./routes/paymentopenpay'));
+app.use('/api/paymentizipay', require('./routes/paymentizipay'));
 app.use('/api/drawpdf', require('./routes/draw-pdf'));
 app.use('/api/billing', require('./routes/billing'));
 app.use('/api/gmaps', require('./routes/gmaps'));
