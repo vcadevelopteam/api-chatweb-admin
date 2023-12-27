fcdc6e2db91a04fa8f22e55ddb0d9449fca1afad
nirvanaGarciaV <nirvagarciav@gmail.com>
2023-12-27 16:43:33 -0500

    fix

--- controllers/gmapsController.js
+++ controllers/gmapsController.js
@@ -486,7 +486,7 @@ exports.findcoordinateinpolygons = async (req, res) => {
       orgid,
       result: modifiedResult,
       inside_schedule,
-      order_datetime: currentDateTime.format('YYYY-MM-DD HH:mm:ss'),
+      order_datetime: order_datetime,
     };
 
     return res.json(response);
