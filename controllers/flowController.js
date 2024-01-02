bc620dc473057877ee5de3ddf56f31c2effeb514
Pablo Rojas <projasb25@gmail.com>
2024-01-02 13:47:48 -0500

    method to uppercase¡

--- controllers/flowController.js
+++ controllers/flowController.js
@@ -202,7 +202,7 @@ exports.bridgeOauth10 = async (req, res) => {
         const { method, url, body } = req.body;
         const finalHeaders = buildAuthOauth(req.headers, url, method.toUpperCase())
 
-        if (method === "POST") {
+        if (method.toUpperCase() === "POST") {
             result = await axios.post(url, body, finalHeaders);
         } else if (method === "PUT") {
             result = await axios.put(url, body, finalHeaders);
