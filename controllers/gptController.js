12afaeaec189ee6fc77ea971ae8a4e55e78c6e34
nirvanaGarciaV <nirvagarciav@gmail.com>
2024-01-24 17:46:46 -0500

    adding axiosObservable in gptController

--- controllers/gptController.js
+++ controllers/gptController.js
@@ -4,15 +4,15 @@ const { axiosObservable, getErrorCode } = require("../config/helpers");
 exports.createThreads = async (req, res) => {
   try {
     const { apikey } = req.body;
-    let responseThread = await axios({
-      method: "post",
-      url: `${process.env.GPT_SERVICES}/threads`,
+    let responseThread = await axiosObservable({
       data: { apikey: apikey },
-      headers: {
+      headers: {   
         Authorization: req.headers.authorization,
-        "Content-Type": "application/json",
+        "Content-Type": "application/json", 
       },
-      timeout: 600000,
+      method: 'post',
+      url: `${process.env.GPT_SERVICES}/threads`,
+      _requestid: req._requestid,
     });
 
     if (responseThread.data && responseThread.statusText === "OK") {
@@ -36,17 +36,17 @@ exports.createThreads = async (req, res) => {
 exports.deleteThreads = async (req, res) => {
   try {
     const { apikey, thread_id } = req.body;
-    let responseDeleteThread = await axios({
-      method: "post",
-      url: `${process.env.GPT_SERVICES}/threads/delete`,
+    let responseDeleteThread = await axiosObservable({
       data: { apikey: apikey, thread_id: thread_id },
-      headers: {
+      headers: {   
         Authorization: req.headers.authorization,
-        "Content-Type": "application/json",
+        "Content-Type": "application/json", 
       },
-      timeout: 600000,
+      method: 'post',
+      url: `${process.env.GPT_SERVICES}/threads/delete`,
+      _requestid: req._requestid,
     });
-
+   
     if (responseDeleteThread.data && responseDeleteThread.statusText === "OK") {
       return res.json(responseDeleteThread.data);
     }
@@ -75,9 +75,8 @@ exports.createAssistant = async (req, res) => {
       retrieval,
       codeinterpreter,
     } = req.body;
-    let reponseCreateAssistant = await axios({
-      method: "post",
-      url: `${process.env.GPT_SERVICES}/assistants/new`,
+
+    let reponseCreateAssistant = await axiosObservable({
       data: {
         apikey: apikey,
         name: name,
@@ -86,11 +85,13 @@ exports.createAssistant = async (req, res) => {
         retrieval: retrieval,
         codeinterpreter: codeinterpreter,
       },
-      headers: {
+      headers: {   
         Authorization: req.headers.authorization,
-        "Content-Type": "application/json",
+        "Content-Type": "application/json", 
       },
-      timeout: 600000,
+      method: 'post',
+      url: `${process.env.GPT_SERVICES}/assistants/new`,
+      _requestid: req._requestid,
     });
 
     if (
@@ -126,9 +127,7 @@ exports.updateAssistant = async (req, res) => {
       codeinterpreter,
       file_ids,
     } = req.body;
-    let responseUpdateAssistant = await axios({
-      method: "post",
-      url: `${process.env.GPT_SERVICES}/assistants/update`,
+    let responseUpdateAssistant = await axiosObservable({
       data: {
         apikey: apikey,
         assistant_id: assistant_id,
@@ -139,13 +138,15 @@ exports.updateAssistant = async (req, res) => {
         codeinterpreter: codeinterpreter,
         file_ids: file_ids,
       },
-      headers: {
+      headers: {   
         Authorization: req.headers.authorization,
-        "Content-Type": "application/json",
+        "Content-Type": "application/json", 
       },
-      timeout: 600000,
+      method: 'post',
+      url: `${process.env.GPT_SERVICES}/assistants/update`,
+      _requestid: req._requestid,
     });
-
+   
     if (
       responseUpdateAssistant.data &&
       responseUpdateAssistant.statusText === "OK"
@@ -170,20 +171,20 @@ exports.updateAssistant = async (req, res) => {
 exports.deleteAssistant = async (req, res) => {
   try {
     const { apikey, assistant_id } = req.body;
-    let responseDeleteAssistant = await axios({
-      method: "post",
-      url: `${process.env.GPT_SERVICES}/assistants/delete`,
+    let responseDeleteAssistant = await axiosObservable({
       data: {
         apikey: apikey,
         assistant_id: assistant_id,
       },
-      headers: {
+      headers: {   
         Authorization: req.headers.authorization,
-        "Content-Type": "application/json",
+        "Content-Type": "application/json", 
       },
-      timeout: 600000,
+      method: 'post',
+      url: `${process.env.GPT_SERVICES}/assistants/delete`,
+      _requestid: req._requestid,
     });
-
+   
     if (
       responseDeleteAssistant.data &&
       responseDeleteAssistant.statusText === "OK"
@@ -208,9 +209,7 @@ exports.deleteAssistant = async (req, res) => {
 exports.messages = async (req, res) => {
   try {
     const { text, assistant_id, thread_id, sources, apikey } = req.body;
-    let responseMessages = await axios({
-      method: "post",
-      url: `${process.env.GPT_SERVICES}/assistants/messages`,
+    let responseMessages = await axiosObservable({
       data: {
         text: text,
         assistant_id: assistant_id,
@@ -218,12 +217,14 @@ exports.messages = async (req, res) => {
         sources: sources,
         apikey: apikey,
       },
-      headers: {
+      headers: {   
         Authorization: req.headers.authorization,
-        "Content-Type": "application/json",
+        "Content-Type": "application/json", 
       },
-      timeout: 600000,
-    });
+      method: 'post',
+      url: `${process.env.GPT_SERVICES}/assistants/messages`,
+      _requestid: req._requestid,
+    }); 
 
     if (responseMessages.data && responseMessages.statusText === "OK") {
       return res.json(responseMessages.data);
@@ -246,21 +247,21 @@ exports.messages = async (req, res) => {
 exports.addFile = async (req, res) => {
   try {
     const { file_url, file_name, apikey } = req.body;
-    let responseAddFile = await axios({
-      method: "post",
-      url: `${process.env.GPT_SERVICES}/files`,
+    let responseAddFile = await axiosObservable({
       data: {
         file_url: file_url,
         file_name: file_name,
         apikey: apikey,
       },
-      headers: {
+      headers: {   
         Authorization: req.headers.authorization,
-        "Content-Type": "application/json",
+        "Content-Type": "application/json", 
       },
-      timeout: 600000,
-    });
-
+      method: 'post',
+      url: `${process.env.GPT_SERVICES}/files`,      
+      _requestid: req._requestid,
+    }); 
+    
     if (responseAddFile.data && responseAddFile.statusText === "OK") {
       return res.json(responseAddFile.data);
     }
@@ -282,21 +283,21 @@ exports.addFile = async (req, res) => {
 exports.assignFile = async (req, res) => {
   try {
     const { assistant_id, file_id, apikey } = req.body;
-    let responseAssignFile = await axios({
-      method: "post",
-      url: `${process.env.GPT_SERVICES}/assistants/files`,
+    let responseAssignFile = await axiosObservable({
       data: {
         assistant_id: assistant_id,
         file_id: file_id,
         apikey: apikey,
       },
-      headers: {
+      headers: {   
         Authorization: req.headers.authorization,
-        "Content-Type": "application/json",
+        "Content-Type": "application/json", 
       },
-      timeout: 600000,
-    });
-
+      method: 'post',
+      url: `${process.env.GPT_SERVICES}/assistants/files`,
+      _requestid: req._requestid,
+    }); 
+  
     if (responseAssignFile.data && responseAssignFile.statusText === "OK") {
       return res.json(responseAssignFile.data);
     }
@@ -318,20 +319,20 @@ exports.assignFile = async (req, res) => {
 exports.verifyFile = async (req, res) => {
   try {
     const { assistant_id, apikey } = req.body;
-    let responseVerifyFile = await axios({
-      method: "post",
-      url: `${process.env.GPT_SERVICES}/assistants/files/list`,
+    let responseVerifyFile = await axiosObservable({
       data: {
         assistant_id: assistant_id,
         apikey: apikey,
       },
-      headers: {
+      headers: {   
         Authorization: req.headers.authorization,
-        "Content-Type": "application/json",
+        "Content-Type": "application/json", 
       },
-      timeout: 600000,
-    });
-
+      method: 'post',
+      url: `${process.env.GPT_SERVICES}/assistants/files/list`,
+      _requestid: req._requestid,
+    }); 
+   
     if (responseVerifyFile.data && responseVerifyFile.statusText === "OK") {
       return res.json(responseVerifyFile.data);
     }
@@ -353,19 +354,19 @@ exports.verifyFile = async (req, res) => {
 exports.deleteFile = async (req, res) => {
   try {
     const { file_id, apikey } = req.body;
-    let responseDeleteFile = await axios({
-      method: "post",
-      url: `${process.env.GPT_SERVICES}/files/delete`,
+    let responseDeleteFile = await axiosObservable({
       data: {
         file_id: file_id,
         apikey: apikey,
       },
-      headers: {
+      headers: {   
         Authorization: req.headers.authorization,
-        "Content-Type": "application/json",
+        "Content-Type": "application/json", 
       },
-      timeout: 600000,
-    });
+      method: 'post',
+      url: `${process.env.GPT_SERVICES}/files/delete`,
+      _requestid: req._requestid,
+    });   
 
     if (responseDeleteFile.data && responseDeleteFile.statusText === "OK") {
       return res.json(responseDeleteFile.data);
@@ -388,20 +389,20 @@ exports.deleteFile = async (req, res) => {
 exports.massiveDelete = async (req, res) => {
   try {
     const { apikey, ids } = req.body;
-    let responseMassiveDelete = await axios({
-      method: "post",
-      url: `${process.env.GPT_SERVICES}/assistants/massivedelete`,
+    let responseMassiveDelete = await axiosObservable({
       data: {
         apikey: apikey,
         ids: ids,
       },
-      headers: {
+      headers: {   
         Authorization: req.headers.authorization,
-        "Content-Type": "application/json",
+        "Content-Type": "application/json", 
       },
-      timeout: 600000,
-    });
-
+      method: 'post',
+      url: `${process.env.GPT_SERVICES}/assistants/massivedelete`,
+      _requestid: req._requestid,
+    });   
+  
     if (
       responseMassiveDelete.data &&
       responseMassiveDelete.statusText === "OK"
