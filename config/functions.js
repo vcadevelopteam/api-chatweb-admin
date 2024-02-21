1d85fb87f3581165807c5248dd99d54e2f762c94
Sebas-Delgado <ronald.delgado@vcaperu.com>
2024-02-21 18:41:04 -0500

    user ins function fixed

--- config/functions.js
+++ config/functions.js
@@ -100,7 +100,7 @@ module.exports = {
         protected: "SELECT"
     },
     UFN_ORGUSER_INS: {
-        query: "SELECT * FROM ufn_orguser_ins($corpid, $orgid, $p_userid, $rolegroups, $usersupervisor, $bydefault, $labels, $groups, $channels, $status,$type, $defaultsort, $username, $operation, $redirect, $storeid, $warehouseid)",
+        query: "SELECT * FROM ufn_orguser_ins($corpid, $orgid, $p_userid, $rolegroups, $usersupervisor, $bydefault, $labels, $groups, $channels, $status,$type, $defaultsort, $username, $operation, $redirect, $storeid, $warehouseid, $showbots)",
         module: "/extras/users",
         protected: "INSERT"
     },
