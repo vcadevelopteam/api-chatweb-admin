6424927e79694426fff3ccf5ea6feafe358c92a1
Carlos Farro <fdcarlosd1@gmail.com>
2024-03-04 19:57:22 -0500

    remove console

--- controllers/corpController.js
+++ controllers/corpController.js
@@ -26,16 +26,9 @@ exports.insCorp = async (req, res) => {
                 throw new Error("Error to get iam token")
             }
             const DNS = await createDNS(token, parameters.domainname, req._requestid);
-            // if (DNS.error) {
-            //     throw new Error("Error to insert DNS")
-            // }
             const pageRule = await createPageRule(token, DNS.result.content, DNS.result.name, req._requestid);
-            // if (pageRule.error) {
-            //     throw new Error("Error to insert pageRule")
-            // }
             succesdomain = true;
             await updateDomainRecaptcha(DNS.result.name)
-            console.log("pageRule", pageRule)
         }
         return res.json({ error: false, success: true, data: result, key, succesdomain });
     }
