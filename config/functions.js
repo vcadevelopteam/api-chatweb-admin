82cf31bbcc8e98b328f23eed27c8e2df7f3bae34
Sebas-Delgado <ronald.delgado@vcaperu.com>
2024-03-11 10:54:03 -0500

    orderlist cancel tipification function added

--- config/functions.js
+++ config/functions.js
@@ -3986,4 +3986,9 @@ module.exports = {
         module: "",
         protected: "INSERT"
     },
+    UFN_UPDATE_ORDERCANCELED: {
+        query: "select * from ufn_update_order_canceled($corpid, $orgid, $orderid, $descriptionreason, $orderstatus, $username)",
+        module: "",
+        protected: "INSERT"
+    },
 }
