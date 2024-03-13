5e1b7d9a030239de693b37c91e6dd79492fd2cf3
Carlos Farro <carlos.farro@vcaperu.com>
2024-03-12 20:14:06 -0500

    its a test

--- controllers/deliveryController.js
+++ controllers/deliveryController.js
@@ -1,5 +1,6 @@
 const { errors, getErrorCode, axiosObservable, setSessionParameters } = require('../config/helpers');
 const { executesimpletransaction } = require('../config/triggerfunctions');
+const https = require('https');
 
 function getDistanceBetweenPoints(point1, point2) {
     var radLat1 = Math.PI * point1.latitude / 180;
@@ -117,7 +118,7 @@ exports.routing = async (req, res) => {
             console.log("ordersGrouped", ordersGrouped)
 
             const res = await executesimpletransaction("UFN_DELIVERYROUTECODE_INS_ARRAY", { ...parameters, orders: JSON.stringify(ordersGrouped) })
-            
+
             return ordersGrouped;
         }))
 
@@ -126,3 +127,54 @@ exports.routing = async (req, res) => {
         return res.status(500).json(getErrorCode(null, exception, `Request to ${req.originalUrl}`, req._requestid));
     }
 }
+
+exports.imaco = async (req, res) => {
+    const data = JSON.stringify({
+        CompanyDB: "SBODEMOUS",
+        Password: "1234",
+        UserName: "manager"
+    });
+
+    const options = {
+        hostname: '190.12.86.123',
+        port: 50000,
+        path: '/b1s/v1/Login',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Cookie': 'ROUTEID=.node3',
+          'Content-Length': data.length
+        },
+        rejectUnauthorized: false, // Ignorar errores de certificado SSL
+        // Forzar el uso de TLS 1.0; solo para diagnóstico o entornos controlados
+        secureProtocol: 'TLSv1_method',
+      };
+
+    try {
+        const request = https.request(options, (response) => {
+            let responseBody = '';
+    
+            response.on('data', (chunk) => {
+                responseBody += chunk;
+            });
+    
+            response.on('end', () => {
+                // Aquí puedes procesar la respuesta recibida
+                console.log(responseBody);
+                // Responder al cliente HTTP original con el resultado
+                res.status(response.statusCode).json(JSON.parse(responseBody));
+            });
+        });
+    
+        request.on('error', (error) => {
+            console.error(error);
+            res.status(500).json({error});
+        });
+    
+        request.write(data);
+        request.end();
+        
+    } catch (exception) {
+        return res.status(500).json(getErrorCode(null, exception, `Request to ${req.originalUrl}`, req._requestid));
+    }
+};
--- routes/delivery.js
+++ routes/delivery.js
@@ -8,4 +8,8 @@ router.post("/routing",
     deliveryController.routing
 )
 
+router.get("/imaco",
+    deliveryController.imaco
+)
+
 module.exports = router;
