3bc4bf289d165731e0045cc459e2f98d44218568
Diego Del Castillo <diego.delcastillo@vcaperu.com>
2024-06-17 15:37:02 -0500

    RLACL139. Plantillas

--- config/functions.js
+++ config/functions.js
@@ -3457,7 +3457,7 @@ module.exports = {
         protected: "SELECT"
     },
     UFN_MESSAGETEMPLATE_RESET: {
-        query: "UPDATE messagetemplate SET status = 'ELIMINADO', changeby = $username, changedate = NOW() WHERE corpid = $corpid AND orgid = $orgid AND type = 'HSM' AND status 'ACTIVO' AND ($communicationchannelid = ANY(string_to_array(communicationchannelid, ',')::bigint[]) OR namespace = $namespace);",
+        query: "UPDATE messagetemplate SET status = 'ELIMINADO', changeby = $username, changedate = NOW() WHERE corpid = $corpid AND orgid = $orgid AND type = 'HSM' AND status = 'ACTIVO' AND ($communicationchannelid = ANY(string_to_array(communicationchannelid, ',')::bigint[]) OR namespace = $namespace);",
         module: "",
         protected: "UPDATE"
     },
