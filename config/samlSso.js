6a52f19de04616bbdf532e7223ed2764a230f928
Carlos Farro <carlos.farro@vcaperu.com>
2024-06-21 18:10:53 -0500

    configurando y separando logica en el .env

--- config/samlSso.js
+++ config/samlSso.js
@@ -3,20 +3,25 @@ const fs = require('fs')
 const path = require('path');
 const SamlStrategy = require('passport-saml').Strategy
 
-const idp = fs.readFileSync(path.resolve(__dirname, 'certs/claro.pem'), 'utf-8');
+// const idp = fs.readFileSync(path.resolve(__dirname, 'certs/claro.pem'), 'utf-8');
 const pvkey = fs.readFileSync(path.resolve(__dirname, 'certs/pvkey.pem'), 'utf-8');
+
+const IDP_CERT = process.env.IPD_SAML_CERT ?? '----';
+const IDP_ENTRYPOINT = process.env.IDP_SAML_ENTRYPOINT ?? ''
+const IDP_ISSUER = process.env.IDP_SAML_ISSUER ?? ''
+const IDP_LOGOUTURL = process.env.IDP_SAML_LOGOUTURL ?? ''
 
 const samlStrategy = new SamlStrategy(
 	{
 		path: '/api/auth/idps/saml20/sso',
-		entryPoint: 'https://devidentidades.claro.com.pe/isam/sps/QuickFed/saml20/login',
+		entryPoint: IDP_ENTRYPOINT,
 		issuer: 'https://apiprd.laraigo.com/api/auth/idps/saml20',
-		idpIssuer: 'https://devidentidades.claro.com.pe/isam/sps/QuickFed/saml20',
+		idpIssuer: IDP_ISSUER,
 		callbackUrl: 'https://stagingapix.laraigo.com/api/auth/idps/saml20/sso',
-		logoutUrl: 'https://devidentidades.claro.com.pe/pkmslogout',
+		logoutUrl: IDP_LOGOUTURL,
 		decryptionPvk: pvkey,
 		privateKey: pvkey,
-		cert: idp,
+		cert: IDP_CERT,
 		signatureAlgorithm: 'sha256',
 		identifierFormat: 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress'
 	},
