4df8727053d7032d37ea22f40b69aaa8150b316b
Carlos Farro <carlos.farro@vcaperu.com>
2024-07-08 13:09:25 -0500

    compare testing

--- controllers/gmapsController.js
+++ controllers/gmapsController.js
@@ -49,10 +49,8 @@ function formatCoordinate(coordinate) {
 function transformKMLtoJSON(kmlObject) {
     const newFeatures = [];
 
-    if (kmlObject.kml && kmlObject.kml.Document && kmlObject.kml.Document[0]) {
-
-        const document = kmlObject.kml.Document[0];
-        const folders = document.Folder || [document];
+    if (kmlObject.kml && kmlObject.kml.Document && kmlObject.kml.Document[0] && kmlObject.kml.Document[0].Folder) {
+        const folders = kmlObject.kml.Document[0].Folder;
 
         folders.forEach((folder, folderIndex) => {
             const folderName = folder.name && folder.name[0];
@@ -60,26 +58,26 @@ function transformKMLtoJSON(kmlObject) {
 
             if (folderName && placemarks) {
                 placemarks.forEach((placemark, index) => {
-                    let nombre = placemark.name && placemark.name[0];
-                    const polygon = placemark.Polygon && placemark.Polygon[0];
-                    const coordinates = polygon && polygon.outerBoundaryIs[0].LinearRing[0].coordinates[0];
-                    
-                    if (nombre && nombre.toLowerCase() === 'poligono kokoriko') {
-                        nombre = 'Reparto Kokoriko San Diego';
-                    }
+                    const nombre = placemark.name && placemark.name[0];
+                    const coordinates = placemark.Polygon && placemark.Polygon[0].outerBoundaryIs[0].LinearRing[0].coordinates[0];
+
+                    console.log('Folder:', folderName);
+                    console.log('Nombre:', nombre);
+                    console.log('Coordinates:', coordinates);
 
                     if (nombre == 'ZONA ROJA - Santa Anita 2') {
                         const x = 2
                     }
-
                     if (nombre && coordinates) {
-                        const storeid = findStoreId(nombre);
-                        const coordenadas = coordinates.split(' ').map(formatCoordinate).filter(coord => coord !== null);                  
+                        const storeid = findStoreId(nombre);  
+                        const coordenadas = coordinates.split(' ').map(formatCoordinate).filter(coord => coord !== null);
+
                         const horario = findSchedule(nombre);
+
                         const newJSON = {
                             id: 0,
                             name: nombre,
-                            storeid: storeid,
+                            storeid: storeid,  
                             schedule: horario,
                             polygons: coordenadas,
                             operation: 'INSERT'
@@ -90,8 +88,6 @@ function transformKMLtoJSON(kmlObject) {
                 });
             }
         });
-    } else {
-        console.log('KML structure does not match expected format');
     }
     console.log('Nuevo JSON:', newFeatures);
     return newFeatures;
