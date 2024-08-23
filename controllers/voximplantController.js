4683191b158adba889968ba61e14dc55ac1fb727
Diego Del Castillo <diego.delcastillo@vcaperu.com>
2024-08-22 19:10:31 -0500

    1.29.0. Voximplant Backup

--- controllers/voximplantController.js
+++ controllers/voximplantController.js
@@ -1780,6 +1780,23 @@ exports.startNextCallTask = async (request, result) => {
     }
 }
 
+exports.getCompleteRecord = async (request, result) => {
+    try {
+        let requestResult = await voximplant.getCallRecord(request.body)
+        if (requestResult)
+            return result.json(requestResult);
+        return result.status(400).json(requestResult)
+    }
+    catch (err) {
+        return result.status(500).json({
+            code: "error_unexpected_error",
+            error: true,
+            message: err.message,
+            success: false,
+        })
+    }
+}
+
 exports.getCallLists = async (request, result) => {
     try {
         let requestResult = await voximplant.getCallLists(request.body)
