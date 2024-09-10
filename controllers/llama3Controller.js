76ee4ccf4047f5e6aab0fcf1d0c926647f35a14b
Carlos Farro <carlos.farro@vcaperu.com>
2024-09-10 10:22:54 -0500

    FIX! add context on query, llama api was changed!

--- controllers/llama3Controller.js
+++ controllers/llama3Controller.js
@@ -308,6 +308,7 @@ exports.query = async (req, res) => {
         system_prompt: system_prompt,
         model: model,
         thread_id: thread_id,
+        context: " ",
         max_new_tokens: max_new_tokens,
         temperature: temperature,
         top_p: top_p
