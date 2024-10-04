764ae7a4ec86fda661271eee1172579d0074d6f6
Chebass <seb.delca.22@gmail.com>
2024-10-04 09:53:43 -0500

    openLink endpoint modified

--- controllers/linkCountController.js
+++ controllers/linkCountController.js
@@ -1,8 +1,8 @@
 const { executesimpletransaction } = require('../config/triggerfunctions');
 
 exports.openLink = async (req, res) => {
-    const { corpid, orgid, messagetemplateid, historyid, type, link_id } = req.body
-    const result = await executesimpletransaction("UFN_REPORTLINK_INS", { corpid, orgid, messagetemplateid, historyid, type, link_id})
+    const { corpid, orgid, messagetemplateid, historyid, type, link_id, username } = req.body
+    const result = await executesimpletransaction("UFN_REPORTLINK_INS", { corpid, orgid, messagetemplateid, historyid, type, link_id, username})
 
     if (!result.error) {
         return res.json({ error: false, success: true});
